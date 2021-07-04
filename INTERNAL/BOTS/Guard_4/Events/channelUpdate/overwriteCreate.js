2f92dcb9d9a68dab7aeeb018ef95895c2aa50dee
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-05 02:26:53 +0300

    xd

--- INTERNAL/BOTS/Guard_4/Events/channelUpdate/overwriteCreate.js
+++ INTERNAL/BOTS/Guard_4/Events/channelUpdate/overwriteCreate.js
@@ -13,6 +13,10 @@ class ChannelUpdate {
         const client = this.client;
         if (curChannel.guild.id !== client.config.server) return;
         const utils = await low(client.adapters('utils'));
+        if (curChannel.parentID === "857667607121756189") await curChannel.updateOverwrite(curChannel.guild.roles.cache.get("857392584056766485"), {
+            VIEW_CHANNEL: true,
+            CONNECT: true
+        });
         const entry = await curChannel.guild.fetchAuditLogs({ type: "CHANNEL_OVERWRITE_CREATE" }).then(logs => logs.entries.first());
         if (entry.createdTimestamp <= Date.now() - 1000) return;
         if (entry.executor.id === client.user.id) return;
