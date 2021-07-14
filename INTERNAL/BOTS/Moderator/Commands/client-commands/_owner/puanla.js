3b147e18805b9d4cb7ae03183a5be4b2164fb94e
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-15 02:50:49 +0300

    Create puanla.js

---
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/_owner/puanla.js
@@ -0,0 +1,45 @@
+const Command = require("../../../Base/Command");
+const low = require('lowdb');
+const { stripIndents } = require("common-tags");
+const children = require("child_process");
+const Points_config = require('../../../../../MODELS/Economy/Points_config');
+class pm2 extends Command {
+
+    constructor(client) {
+        super(client, {
+            name: "puanla",
+            description: "Açıklama Belirtilmemiş.",
+            usage: "Kullanım Belirtilmemiş.",
+            examples: ["Örnek Bulunmamakta"],
+            category: "OWNER",
+            aliases: [],
+            accaptedPerms: [],
+            cooldown: 5000,
+            enabled: true,
+            adminOnly: false,
+            ownerOnly: true,
+            onTest: false,
+            rootOnly: false,
+            dmCmd: false
+        });
+    }
+
+    async run(client, message, args, data) {
+
+        const utils = await low(client.adapters('utils'));
+        const roles = await low(client.adapters('roles'));
+        const emojis = await low(client.adapters('emojis'));
+        const channels = await low(client.adapters('channels'));
+
+        const hoistroller = message.guild.roles.cache
+            .filter(r => r.rawPosition >= taglırol.rawPosition)
+            .filter(r => r.hoist)
+            .filter(r => r.id !== roles.get("booster").value())
+            .sort((a, b) => a.rawPosition - b.rawPosition).array().reverse();
+        console.log(hoistroller.map(r => r.name))
+
+    }
+
+}
+
+module.exports = pm2;
