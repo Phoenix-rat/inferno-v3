d7920d66ac4ea242fce0e4153064b081eab61f49
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-07-17 02:18:58 +0300

    d

---
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/General/say2.js
@@ -0,0 +1,41 @@
+const { MessageEmbed } = require('discord.js');
+const Command = require("../../../Base/Command");
+const low = require('lowdb');
+const { stripIndent } = require('common-tags');
+class Say2 extends Command {
+
+    constructor(client) {
+        super(client, {
+            name: "say2",
+            description: "Sunucunun anlık bilgisini verir.",
+            usage: "say2",
+            examples: ["saylan"],
+            category: "Genel",
+            accaptedPerms: ["cmd-registry", "cmd-double", "cmd-single", "cmd-ceo"],
+            cooldown: 10000
+        });
+    }
+
+    async run(client, message, args) {
+        const emojis = await low(client.adapters('emojis'));
+        const channels = await low(client.adapters('channels'));
+        const roles = await low(client.adapters('roles'));
+
+        let böyle = message.guild.memberCount;
+        let gitme = await message.guild.members.cache.filter(m => client.config.tag.some(tag => m.user.username.includes(tag))).size;
+        let ağlarım = await message.guild.members.cache.filter(m => m.presence.status !== 'offline').size;
+        let relax = await message.guild.members.cache.filter(m => m.roles.cache.has(roles.get("booster").value())).size;
+        let baby = await message.guild.voiceStates.cache.filter(v => v.channel).size;
+        
+        const saranembed = new MessageEmbed().setTimestep().setColor("BLACK").setFooter("Kahve ❤ † INFEЯИO").setTitle("† INFEЯИO Ses Bilgileri")
+    
+        await message.channel.send(saranembed .setDescription(stripIndent`
+       ${emojis.get("kahvehac").value()} Sunucuda Toplam ${rain(client, böyle)} Üye Bulunmaktadır.
+       ${emojis.get("kahvehac").value()} Sunucuda Toplam Aktif ${rain(client, ağlarım)} Üye Bulunmaktadır.
+       ${emojis.get("kahvehac").value()} Sunucudaki Toplam Taglı ${rain(client, gitme)} Üye Bulunamktadır.
+       ${emojis.get("kahvehac").value()} Ses Kanallarında ${rain(client, baby)} Üye Bulunmaktadır.
+        `));
+    }
+}
+
+module.exports = Say2;
