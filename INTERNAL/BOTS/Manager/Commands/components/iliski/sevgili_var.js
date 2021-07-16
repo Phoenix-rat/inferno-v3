08f5fc6c5440362840810b6bdd4d7a63f6cb73a3
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-17 02:59:19 +0300

    xd

--- INTERNAL/BOTS/Manager/Commands/components/iliski/sevgili_var.js
+++ INTERNAL/BOTS/Manager/Commands/components/iliski/sevgili_var.js
@@ -26,11 +26,19 @@ class RolSeçim extends Component {
         const guild = client.guilds.cache.get(ctx.guildID);
         const mentioned = guild.members.cache.get(ctx.user.id);
         const myRol = guild.roles.cache.get(roles.get("sevgili_var").value());
-        await mentioned.roles.remove(Object.keys(roles.value()).filter(key => key.startsWith("sevgili_")).map(key => roles.get(key).value()));
         await mentioned.roles.add(myRol.id);
-        return await ctx.send(`${myRol.name} rolü üzerinize verildi`, {
-            ephemeral: true
-        });
+        if (mentioned.roles.cache.has(myRol.id)) {
+            await mentioned.roles.remove(myRol.id);
+            return await ctx.send(`${myRol.name} rolü üzerinizden alındı`, {
+                ephemeral: true
+            });
+        } else {
+            await mentioned.roles.remove(Object.keys(roles.value()).filter(key => key.startsWith("sevgili_")).map(key => roles.get(key).value()));
+            await mentioned.roles.add(myRol.id);
+            return await ctx.send(`${myRol.name} rolü üzerinize verildi`, {
+                ephemeral: true
+            });
+        }
 
 
 
