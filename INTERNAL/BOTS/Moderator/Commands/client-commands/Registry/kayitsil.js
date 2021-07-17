303e5033fa6b2c66bd7ff577900f5c8ea7ca333d
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-07-17 14:53:18 +0300

    Update kayitsil.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Registry/kayitsil.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Registry/kayitsil.js
@@ -22,15 +22,15 @@ class KayitSil extends Command {
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
         const mentioned = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-        if (!mentioned) return message.channel.send(new Discord.MessageEmbed().setDescription(`${emojis.get("kullaniciyok").value()} Kullanıcı bulunamadı!`).setColor('#2f3136'));
+        if (!mentioned) return message.channel.send(new Discord.MessageEmbed().setDescription(`Kullanıcı bulunamadı!`).setColor('BLACK'));
+
         const data = await nameData.findOne({ _id: mentioned.user.id });
         if (!data) return message.channel.send(new Discord.MessageEmbed().setDescription('Veri bulunamadı').setColor('RED'));
-        if (message.member.roles.highest.rawPosition <= mentioned.roles.highest.rawPosition) return message.channel.send(new Discord.MessageEmbed().setColor('#2f3136').setDescription(`${emojis.get("missingPerms").value()} Bunu yapmak için yeterli yetkiye sahip değilsin`));
+        if (message.member.roles.highest.rawPosition <= mentioned.roles.highest.rawPosition) return message.channel.send(new Discord.MessageEmbed().setColor('#2f3136').setDescription(`Bunu yapmak için yeterli yetkiye sahip değilsin`));
         await nameData.deleteOne({ _id: mentioned.user.id });
         await mentioned.roles.remove(mentioned.roles.cache.filter(r => r.editable).array());
         await mentioned.roles.add(roles.get('welcome').value());
         await message.channel.send(new Discord.MessageEmbed().setDescription("Kullanıcının verileri başarıyla silindi").setColor('#2f3136'));
-        await message.guild.channels.cache.get(channels.get("mod-registry").value()).send(new Discord.MessageEmbed().setDescription(`${message.member} yetkilisi ${mentioned} kişisinin kayıt verilerini sildi.`));
     }
 }
 module.exports = KayitSil;
