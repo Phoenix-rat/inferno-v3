8b1989fc5e359502e44519c5cd94b8cc82471f1f
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-17 05:11:25 +0300

    Update say2.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/General/say2.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/General/say2.js
@@ -44,7 +44,7 @@ class Say2 extends Command {
             [undefined]: "Diğer"
         }
         const sesler = Object.keys(obj).filter(k => obj[k] >= 10).sort((a, b) => obj[b] - obj[a]).slice(0, 3);
-        const deyim = sesler.map(k => `${emojis.get("kahvehac").value()} ${lang[k]} kanallarında \`${obj[k]}\` kişi bulunmaktadır.`).join('\n\n');
+        const deyim = sesler.map(k => `${emojis.get("kahvehac").value()} ${lang[k]} kanallarında \`${obj[k]}\` kişi bulunmaktadır.`).join('\n');
         await message.channel.send(saranembed.setDescription(stripIndent`
        ${emojis.get("kahvehac").value()} Sunucuda \`${böyle}\` üye var.
        ${emojis.get("kahvehac").value()} Aktif olarak \`${ağlarım}\` üye var.
