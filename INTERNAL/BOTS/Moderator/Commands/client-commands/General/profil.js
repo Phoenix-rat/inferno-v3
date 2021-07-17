0fe3285ba55781e7867a627633047325e16a2a46
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-17 04:11:57 +0300

    Update profil.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/General/profil.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/General/profil.js
@@ -50,7 +50,7 @@ class Anonim extends Command {
 
          **❯ Kayıt Bilgisi**
 
-         Kayıt eden kullanıcı: ${profildata ? message.guld.members.cache.get(profildata.executor) : "Bulunamadı"}
+         Kayıt eden kullanıcı: ${profildata ? message.guild.members.cache.get(profildata.executor) : "Bulunamadı"}
          Kayıt olma tarihi: ${profildata ? checkDays(profildata.created) : "Bilinmiyor"}
          Kayıt olma bilgileri: ${profildata ? `${profildata.name} ${profildata.age} - ${profildata.sex}` : "Bulunamadı"}
         `).setThumbnail(mentioned.user.displayAvatarURL({ dynamic: true })).setColor(mentioned.displayHexColor).setTitle("† Dante's INFEЯИO");
