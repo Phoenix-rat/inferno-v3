b8ff5ba7c6d1115bf2acc005c4dab5c7c18077fa
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-17 04:47:37 +0300

    Update say2.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/General/say2.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/General/say2.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require('discord.js');
 const Command = require("../../../Base/Command");
 const low = require('lowdb');
 const { stripIndent } = require('common-tags');
-const { rain } = require("../../../../../HELPERS/functions");
+const { rain, getPath } = require("../../../../../HELPERS/functions");
 
 class Say2 extends Command {
 
@@ -28,15 +28,27 @@ class Say2 extends Command {
         let ağlarım = await message.guild.members.cache.filter(m => m.presence.status !== 'offline').size;
         let relax = await message.guild.members.cache.filter(m => m.roles.cache.has(roles.get("booster").value())).size;
         let baby = await message.guild.voiceStates.cache.filter(v => v.channel).size;
-        
-        const saranembed = new MessageEmbed().setColor("BLACK").setThumbnail(`message.member.`).setFooter("Kahve ❤ † INFEЯИO").setTitle("† INFEЯИO Ses Bilgileri")
-    
-        await message.channel.send(saranembed .setDescription(stripIndent`
-       ${emojis.get("kahvehac").value()} Sunucuda Toplam ${rain(client, böyle)} Üye Bulunmaktadır.
 
-       ${emojis.get("kahvehac").value()} Sunucuda Toplam Aktif ${rain(client, ağlarım)} Üye Bulunmaktadır.
+        const saranembed = new MessageEmbed().setColor("BLACK").setThumbnail(`message.member.`).setFooter("Kahve ❤ † INFEЯИO").setTitle("† INFEЯИO Ses Bilgileri");
+        const obj = {};
+        for (let index = 0; index < message.guild.channels.cache.array().length; index++) {
+            const myChannel = message.guild.channels.cache.array()[index];
+            const key = obj[getPath(channels.value(), myChannel.parentID)];
+            if (!key) {
+                obj[getPath(channels.value(), myChannel.parentID)] = 0;
+            } else {
+                obj[getPath(channels.value(), myChannel.parentID)] = key + 1
+            }
+        }
+        console.log(obj);
 
-       ${emojis.get("kahvehac").value()} Sunucudaki Toplam Taglı ${rain(client, gitme)} Üye Bulunamktadır.
+
+        await message.channel.send(saranembed.setDescription(stripIndent`
+       ${emojis.get("kahvehac").value()} Sunucuda ${rain(client, böyle)} üye var.
+
+       ${emojis.get("kahvehac").value()} Aktif olarak ${rain(client, ağlarım)} üye var.
+
+       ${emojis.get("kahvehac").value()} Tagımızı taşıyarak bize destek olan ${rain(client, gitme)} üye var.
 
        ${emojis.get("kahvehac").value()} Ses Kanallarında ${rain(client, baby)} Üye Bulunmaktadır.
         `));
