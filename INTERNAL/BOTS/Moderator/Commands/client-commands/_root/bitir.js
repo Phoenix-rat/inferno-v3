c66eea479a23e244639211afd8b1a392dcfa769a
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-17 05:44:16 +0300

    Create bitir.js

---
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/_root/bitir.js
@@ -0,0 +1,42 @@
+const Command = require("../../../Base/Command");
+const low = require('lowdb');
+const Discord = require('discord.js');
+const izin = require('../../../../../MODELS/Temprorary/Permissions');
+const keyz = require('shortid');
+class Kur extends Command {
+
+    constructor(client) {
+        super(client, {
+            name: "bitir",
+            description: "Açıklama Belirtilmemiş.",
+            usage: "Kullanım Belirtilmemiş.",
+            examples: ["Örnek Bulunmamakta"],
+            category: "OWNER",
+            aliases: ["başlat"],
+            acceptedRoles: [],
+            cooldown: 5000,
+            enabled: true,
+            adminOnly: false,
+            ownerOnly: false,
+            onTest: false,
+            rootOnly: true,
+            dmCmd: false
+        });
+    }
+
+    async run(client, message, args, data) {
+
+        const utils = await low(client.adapters('utils'));
+        const roles = await low(client.adapters('roles'));
+        const emojis = await low(client.adapters('emojis'));
+        const channels = await low(client.adapters('channels'));
+
+        const publicCat = message.guild.channels.cache.filter(c => c.type === "category").array();
+        const parent = message.guild.channels.cache.get(message.channel.parentID);
+        await parent.setPosition(publicCat.length - 1);
+
+    }
+
+}
+
+module.exports = Kur;
