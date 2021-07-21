1f35c03558f15f3e8c6aa175ab00f90e42e2ef02
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-22 00:13:38 +0300

    Update message.js

--- INTERNAL/BOTS/Moderator/Events/client-events/message.js
+++ INTERNAL/BOTS/Moderator/Events/client-events/message.js
@@ -166,7 +166,7 @@ module.exports = class {
         if (time && (time > Date.now())) return message.channel.send(`${emojis.get("dmcmd").value()} Komutu tekrar kullanabilmek için lütfen **${Math.ceil((time - Date.now()) / 1000)}** saniye bekle!`);
 
         client.logger.log(`[(${message.author.id})] ${message.author.username} ran command [${cmd.info.name}]`, "cmd");
-        if (message.channel.id === "857659757233700875" && message.member.permissions.has("MANAGE_ROLES") && command !== "tag") return;
+        if (message.channel.id === "857659757233700875" && !message.member.permissions.has("MANAGE_ROLES") && command !== "tag") return;
         try {
             cmd.run(client, message, args);
         } catch (e) {
