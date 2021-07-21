3d0868240f0aab67c65fea1a3bcbe37d36666eb6
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-07-21 23:39:12 +0300

    Create kilit2.js

---
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Manager/kilit2.js
@@ -0,0 +1,42 @@
+const Command = require("../../../Base/Command");
+const Discord = require("discord.js");
+const low = require('lowdb');
+class Lock extends Command {
+
+    constructor(client) {
+        super(client, {
+            name: "lock",
+            description: "Mesajın atıldığı kanalı kilitler",
+            usage: "lock",
+            examples: ["lock"],
+            cooldown: 3600000,
+            category: "Düzen",
+            accaptedPerms: ["cmd-single", "cmd-double", "cmd-ceo"]
+        });
+    }
+
+    async run(client, message, args) {
+
+        const utils = await low(client.adapters('utils'));
+        const roles = await low(client.adapters('roles'));
+        const emojis = await low(client.adapters('emojis'));
+        const channels = await low(client.adapters('channels'));
+
+        const everyone = message.channel.permissionsFor(message.guild.id).has("SEND_MESSAGES");
+
+        message.channel.send(
+            new MessageEmbed()
+                .setColor("RANDOM")
+                .setAuthor(message.author.tag, message.author.avatarURL({ dynamic: true }))
+                .setDescription(`Başarılı bir şekilde kanal \`${everyone ? "kilitlendi" : "açıldı"}!\``)
+        );
+        message.channel.updateOverwrite(message.guild.id, {
+            SEND_MESSAGES: !everyone,
+        });
+        await message.react(emojis.get("ok").value().split(':')[2].replace('>', ''));
+
+    }
+
+}
+
+module.exports = Lock;
