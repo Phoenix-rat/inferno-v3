7275052bb550fea520ca9c75228707d9e5289f3b
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-07-21 22:49:22 +0300

    f

--- INTERNAL/BOTS/Moderator/Commands/client-commands/General/avatar.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/General/avatar.js
@@ -19,7 +19,7 @@ class Avatar extends Command {
         const emojis = await low(client.adapters('emojis'));
         let mentioned = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
         if (!mentioned) return message.channel.send(new Discord.MessageEmbed().setDescription(`${emojis.get("kullaniciyok").value()} Kullanıcı bulunamadı!`).setColor('#2f3136'));
-        message.channel.send(new Discord.MessageEmbed().setColor('#2f3136').setImage(mentioned.user.displayAvatarURL({ dynamic: true, size: 4096 })));
+        message.channel.send(`${mentioned.user.displayAvatarURL({dynamic: true ,  size: 4096 })}`)
     }
 }
 
