0492078fab09bec46e0e3ad4ab8edaa318c16f7c
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-07-23 14:49:18 +0300

    Update nerede2.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/General/nerede2.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/General/nerede2.js
@@ -25,22 +25,19 @@ class Where extends Command {
         const mentioned = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         if (!mentioned) return message.channel.send(new Discord.MessageEmbed().setDescription(`${emojis.get("kullaniciyok").value()} Kullanıcı bulunamadı!`).setColor('BLACK'));
         let desu = ``;
-        let voiceinfo = `
-        • Mikrofonu: ${mentioned.voice.mute ? `Kapalı` : `Açık`}
-        • Kulaklığı: ${mentioned.voice.deaf ? `Kapalı` : `Açık`}`
+        let voiceinfo = `• Mikrofonu: ${mentioned.voice.mute ? `Kapalı` : `Açık`} \n • Kulaklığı: ${mentioned.voice.deaf ? `Kapalı` : `Açık`}`
         if (!mentioned.voice.channel) {
             desu = `Belirtilen kullanıcı hiçbir kanalda bulunmamaktadır.`;
         } else {
             desu = `${mentioned.voice.channel} \`${mentioned.voice.channel.members.size}/${mentioned.voice.channel.userLimit}\``;
         }
         let lmc = message.guild.channels.cache.get(mentioned.lastMessageChannelID);
-        if (!lmc) lmc = `En son Mesaj yazdığı kanal bulunamadı`;
-        const embed = new Discord.MessageEmbed().setColor(mentioned.displayHexColor).setFooter(`(${lmc} En son mesaj yazdığı kanal.)`).setAuthor(message.author.tag, message.author.avatarURL({ dynamic: true }));
+        let stfu = `${mentioned.lastMessageChannelID ? `En son mesaj yazdığı kanal.` : `En son Mesaj yazdığı kanal bulunamadı.`}`
+        const embed = new Discord.MessageEmbed().setColor(mentioned.displayHexColor).setFooter(`(${lmc} ${stfu})`).setAuthor(message.author.tag, message.author.avatarURL({ dynamic: true }));
         const neredembed = embed.setDescription(`
         ${mentioned} kişisi **${desu}** kanalında.
          ** • Ses Biglileri:**
-        \`\`\`
-        ${voiceinfo}\`\`\` 
+        \`\`\`${voiceinfo}\`\`\` 
         **• Kanala gitmek için ${mentioned.voice.channel}'a tıklaya bilirsin.**`)
         await message.channel.send(neredembed).then(msg => msg.delete({ timeout: 120000 }));
     }
