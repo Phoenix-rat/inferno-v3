61cb06471fc65a79bf8681f703a39fbb88e5c611
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-07-24 01:08:25 +0300

    j

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Registry/kiz.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Registry/kiz.js
@@ -25,7 +25,7 @@ class Kiz extends Command {
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
         let mentioned = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-        if (!mentioned) return message.channel.send(new Discord.MessageEmbed().setDescription(`${emojis.get("kullaniciyok").value()} Kullanıcı bulunamadı!`).setColor('#2f3136'));
+        if (!mentioned) return message.channel.send(new Discord.MessageEmbed().setDescription(`${emojis.get("kullaniciyok").value()} Kullanıcı bulunamadı!`).setColor('BLACK'));
         if (!mentioned.roles.cache.has(roles.get("welcome").value()) && (mentioned.roles.cache.size > 1)) return message.channel.send(new Discord.MessageEmbed().setDescription(`Sanırım bu üye zaten kayıtlı!`));
         if (utils.get("taglıAlım").value() && !mentioned.user.username.includes(client.config.tag)) {
             if (!mentioned.roles.cache.has(roles.get("vip").value()) && !mentioned.roles.cache.has(roles.get("booster").value())) {
