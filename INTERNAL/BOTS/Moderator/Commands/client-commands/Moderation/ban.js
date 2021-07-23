dfd5a4f71aa84722e0b450c8f19ebfd2815a0a50
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-07-24 00:55:18 +0300

    Update ban.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Moderation/ban.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Moderation/ban.js
@@ -11,7 +11,7 @@ class Ban extends Command {
             examples: ["ban 479293073549950997 10 botları kötü yapıyor"],
             category: "Moderasyon",
             aliases: ["yargı", "infaz"],
-            accaptedPerms: ["root", "cmd-double", "cmd-single", "cmd-ceo"],
+            accaptedPerms: ["root", "owner", "cmd-single", "cmd-ceo"],
             cooldown: 10000
         })
     }
