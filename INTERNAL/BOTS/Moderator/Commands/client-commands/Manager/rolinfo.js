6cbbf9e0f3ea9bf28f7bb691dba8ab95411ab004
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-07-25 13:24:10 +0300

    g

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Manager/rolinfo.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Manager/rolinfo.js
@@ -23,13 +23,12 @@ class RoleInfo extends Command {
         const roles = await low(client.adapters('roles'));
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
-        
         let mentionedRole = message.mentions.roles.first() || message.guild.roles.cache.get(args[0])
         if (!mentionedRole) return message.channel.send(new Discord.MessageEmbed()
         .setColor("RANDOM")
-        .setDescription("Belirtilen rolü sunucuda bulamadım lütfen rolü etiketleyiniz veya ID sini giriniz!"))
- 
-        let mentionedRoleMembers = mentionedRole.members.map(role => `<@${role.id}> - (\`${role.id}\`) `)
+        .setFooter("Kahvelendin <3")
+        .setDescription("Lütfen rolü etiketleyiniz veya ID sini giriniz!"))
+        let mentionedRoleMembers = mentionedRole.members.map(role => `<@${role.id}> (\`${role.id}\`) `)
         message.channel.send(`${mentionedRole} rolündeki üyeler.
         
         ${mentionedRoleMembers.join("\n")})`, { split: true })
