376ac8e5c0ed81c96a0b7f05836a5cf5eba4f130
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-07-25 12:32:41 +0300

    l

--- INTERNAL/BOTS/Moderator/Commands/client-commands/General/lust.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/General/lust.js
@@ -27,12 +27,12 @@ class Lust extends Command {
         
         if (!mentioned) return message.channel.send(new Discord.MessageEmbed()
         .setDescription(`${emojis.get("kullaniciyok").value()} Kişiyi bulunamadım.`).setColor('RANDOM'));
-        if (!mentioned.roles.cache.has(roles.get("starter").value().concat(roles.get("cmd-crew").value()))) {
+        if (!mentioned.roles.cache.has(roles.get("Lust").value())) {
             await message.channel.send(embed
                 .setDescription(`${mentioned} kişisine "**Lust**" yetkisine çıkardım!`));
-            await mentioned.roles.add(roles.get("starter").value().concat(roles.get("cmd-crew").value()));
+            await mentioned.roles.add(roles.get("Lust"));
         } else {
-            await mentioned.roles.remove(roles.get("starter").value().concat(roles.get("cmd-crew").value()));
+            await mentioned.roles.remove(roles.get("Lust").value());
             await message.channel.send(embed
             .setDescription(`${mentioned} kişisini "**Lust**" yetkisinden indirdim!`));
         }
