94fccd07968365ef543c96cca21933b12d7691a6
Stark <51216503+starktwo@users.noreply.github.com>
2021-07-25 21:27:19 +0300

    Update unjail.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Moderation/unjail.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Moderation/unjail.js
@@ -20,14 +20,15 @@ class unJail extends Command {
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
         let mentioned = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+         if(mentioned) return message.reply(`${message.author.id}, ${mentioned.id}`)
+
         if (!mentioned) return message.channel.send(new MessageEmbed().setDescription(`${emojis.get("kullaniciyok").value()} Kullanıcı bulunamadı!`).setColor('#2f3136'));
         const Data = await Jails.findOne({ _id: mentioned.user.id });
-        if (!Data) return message.channel.send(new MessageEmbed().setDescription(`${emojis.get("notfound").value()} Kayt Bulunamadı!`));
+        if (!Data) return message.channel.send(new MessageEmbed().setDescription(`${emojis.get("notfound").value()} Kayıt Bulunamadı!`));
         await mentioned.roles.add(Data.roles.map(rname => message.guild.roles.cache.find(role => role.name === rname)));
         await mentioned.roles.remove(roles.get("prisoner").value());
         await Jails.deleteOne({ _id: mentioned.user.id });
         await message.react(emojis.get("ok").value().split(':')[2].replace('>', ''));
-        message.reply(`${message.author.id}`)
   //      this.client.cmdCooldown[message.author.id][this.info.name] = Date.now() + this.info.cooldown;
         const logChannel = message.guild.channels.cache.get(channels.get("cmd-mod").value());
         const embed = new MessageEmbed().setColor('#2f3136').setDescription(`${emojis.get("unjail").value()} ${mentioned} kullanıcısı jail'i ${message.member} tarafından kaldırıldı!`);
