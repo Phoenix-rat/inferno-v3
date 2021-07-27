7bca2fe763c6727399863b81c3a5616e3c0a9b8b
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-27 16:33:48 +0300

    Update kur.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
@@ -2,6 +2,8 @@ const Command = require("../../../Base/Command");
 const low = require('lowdb');
 const Discord = require('discord.js');
 const Gm = require("gm");
+const Canvas = require('canvas');
+const GIFEncoder = require('gifencoder');
 const fs = require('fs');
 class Kur extends Command {
 
@@ -25,22 +27,36 @@ class Kur extends Command {
     }
 
     async run(client, message, args, data) {
-
         const utils = await low(client.adapters('utils'));
         const roles = await low(client.adapters('roles'));
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
+        const encoder = new GIFEncoder(1000, 400);
+        const fs = require('fs');
+        encoder.start();
+        encoder.setRepeat(-1);   // 0 for repeat, -1 for no-repeat
+        encoder.setDelay(1);  // frame delay in ms
+        encoder.setQuality(10); // image quality. 10 is default.
         const pngFiles = fs.readdirSync(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/`).map(str => str.split('.')[0]).sort((a, b) => Number(a) - Number(b));
-        const PNG = fs.readFileSync(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/${args[0] ? Number(args[0]) : pngFiles.length - 1}.png`)
-        let curGm = Gm(PNG).setFormat('gif').loop(1);
         for (let index = 0; index < (args[0] ? Number(args[0]) : pngFiles.length - 1); index++) {
-            curGm = curGm.delay(1).in([`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/${pngFiles[index]}.png`]);
+    
+            // use node-canvas
+            const canvas = Canvas.createCanvas(1000, 400);
+            const ctx = canvas.getContext('2d');
+            // Since the image takes time to load, you should await it
+            const background = await Canvas.loadImage(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/${index}.png`);
+            // This uses the canvas dimensions to stretch the image onto the entire canvas
+            context.drawImage(background, 0, 0, canvas.width, canvas.height);
+            // Wait for Canvas to load the image
+            const avatar = await Canvas.loadImage(message.author.displayAvatarURL({ format: 'jpg' }));
+            // Draw a shape onto the main canvas
+            context.drawImage(avatar, 25, 25, 200, 200);    
+            encoder.addFrame(ctx);
         }
-        curGm.toBuffer(async (error, buffer) => {
-            if (error) return console.log(error);
-            const att = new Discord.MessageAttachment(buffer, 'pointBar.gif');
-            await message.channel.send(new Discord.MessageEmbed().setImage('attachment://pointBar.gif').attachFiles(att));
-        });
+        encoder.finish();
+        const buffer = encoder.out.getData();
+        const attachment = new Discord.MessageAttachment(buffer, 'my-points.png');
+        await message.channel.send(attachment);
 
     }
 
