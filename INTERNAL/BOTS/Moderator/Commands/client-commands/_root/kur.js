8cb6fc0972361ccd81e09fa48b50a2af86f5a732
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-27 13:43:51 +0300

    xd

--- INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
@@ -3,6 +3,8 @@ const low = require('lowdb');
 const Discord = require('discord.js');
 const Gm = require("gm");
 const fs = require('fs');
+const canvas = require('canvas');
+const Encoder = require('gifencoder');
 class Kur extends Command {
 
     constructor(client) {
@@ -30,11 +32,11 @@ class Kur extends Command {
         const roles = await low(client.adapters('roles'));
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
-        const PNG = fs.readFileSync(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/0.png`)
+        const PNG = fs.readFileSync(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/0.png`);
         let curGm = Gm(PNG).setFormat('gif');
         const pngFiles = fs.readdirSync(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/`).map(str => str.split('.')[0]).sort((a, b) => Number(a) - Number(b));
         for (let index = 1; index < (args[0] ? Number(args[0]) : pngFiles.length); index++) {
-            curGm = curGm.in([`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/${pngFiles[index]}.png`]).delay(1);
+            curGm = curGm.setDraw(message.author.displayAvatarURL({ dynamic: true, type: 'gif' }), 0, 0, "floodfill").in([`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/${pngFiles[index]}.png`]).delay(1);
         }
         for (let index = 0; index < 10; index++) {
             curGm = curGm.in([`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/${args[0] ? Number(args[0]) : pngFiles.length}.png`]).delay(10);
