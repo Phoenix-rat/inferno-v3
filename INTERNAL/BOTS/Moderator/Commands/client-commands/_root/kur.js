cd520e7a717295c079de48fd10893da31c707469
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-27 20:59:55 +0300

    Update kur.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
@@ -31,12 +31,14 @@ class Kur extends Command {
         const roles = await low(client.adapters('roles'));
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
-        const encoder = new GIFEncoder(1000, 400);
-        const fs = require('fs');
+        const encoder = new GIFEncoder(1000, 400);;
         encoder.start();
         encoder.setRepeat(-1);   // 0 for repeat, -1 for no-repeat
         encoder.setDelay(1);  // frame delay in ms
         encoder.setQuality(10); // image quality. 10 is default.
+        // use node-canvas
+        const canvas = Canvas.createCanvas(1000, 400);
+        const context = canvas.getContext('2d');
         const pngFiles = fs.readdirSync(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/`).map(str => str.split('.')[0]).sort((a, b) => Number(a) - Number(b));
         for (let index = 0; index < (args[0] ? Number(args[0]) : pngFiles.length - 1); index++) {
             let file;
@@ -47,22 +49,15 @@ class Kur extends Command {
             } finally {
                 if (file) await file.close();
             }
-            // use node-canvas
-            const canvas = Canvas.createCanvas(1000, 400);
-            const context = canvas.getContext('2d');
-            // Since the image takes time to load, you should await it
             const background = await Canvas.loadImage(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/${pngFiles[index]}.png`);
-            // This uses the canvas dimensions to stretch the image onto the entire canvas
             context.drawImage(background, 0, 0, canvas.width, canvas.height);
-            // Wait for Canvas to load the image
             const avatar = await Canvas.loadImage(message.author.displayAvatarURL({ format: 'jpg' }));
-            // Draw a shape onto the main canvas
-            context.drawImage(avatar, 25, 25, 200, 200);
+            context.drawImage(avatar, 50, 50, 200, 200);
             encoder.addFrame(context);
         }
         encoder.finish();
         const buffer = encoder.out.getData();
-        const attachment = new Discord.MessageAttachment(buffer, 'my-points.png');
+        const attachment = new Discord.MessageAttachment(buffer, 'my-points.gif');
         await message.channel.send(attachment);
 
     }
