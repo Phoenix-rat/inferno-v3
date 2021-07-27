1d20fec4cce6849d0b66e8f83a0d0a1c78a06aba
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-28 00:19:34 +0300

    Update kur.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
@@ -40,8 +40,8 @@ class Kur extends Command {
         const canvas = Canvas.createCanvas(1000, 400);
         const context = canvas.getContext('2d');
         const pngFiles = fs.readdirSync(`/home/winner/inferno-v3/INTERNAL/SRC/point_items/`).map(str => str.split('.')[0].slice(2)).sort((a, b) => Number(a) - Number(b));
+        const myGm = Gm(message.author.displayAvatarURL({ format: 'gif' }));
         for (let index = 0; index < (args[0] ? Math.round(Number(args[2]) / 4) : pngFiles.length); index++) {
-            const myGm = Gm(message.author.displayAvatarURL({ format: 'gif' }));
             console.log(myGm);
             let file;
             try {
