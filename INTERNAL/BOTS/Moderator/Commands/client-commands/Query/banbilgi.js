ac75c6030c189a476fc418eb656d033178c25ed4
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-07-27 18:18:18 +0300

    g

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Query/banbilgi.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Query/banbilgi.js
@@ -12,7 +12,7 @@ class BanSorgu extends Command {
             usage: "banbilgi etiket/id",
             examples: ["banbilgi 674565119161794560"],
             category: "Sorgu",
-            aliases: ["bbilgi"],
+            aliases: ["bbilgi","baninfo"],
             accaptedPerms: ["root", "owner", "cmd-ceo", "cmd-double", "cmd-single", "cmd-ban"],
             cooldown: 10000
         })
@@ -20,16 +20,16 @@ class BanSorgu extends Command {
     async run(client, message, args) {
         const emojis = await low(client.adapters('emojis'));
         const banInfo = await message.guild.fetchBan(args[0]);
-        if (!banInfo) return message.channel.send(new Discord.MessageEmbed().setDescription(`${emojis.get("warn").value()} Belirtilen **ID*'ye sahip bir banlı kullanıcı bulunamadı.`));
+        if (!banInfo) return message.channel.send(new Discord.MessageEmbed().setColor("BLACK").setDescription(`${emojis.get("warn").value()} Belirtilen **ID*'ye sahip bir banlı kullanıcı bulunamadı.`));
         const banData = await Bans.findOne({ _id: args[0] });
         const embed = new Discord.MessageEmbed().setDescription(stripIndent`
-        • Banlanan Kullanıcı: ${banInfo.user} (${banInfo.user.tag} - ${banInfo.user.id})
+        • Banlanan Kullanıcı: ${banInfo.user} (\`${banInfo.user.tag}\` - \`${banInfo.user.id}\`)
         • Banlanma sebebi: \`${banData ? banData.reason : "Sebeb Belirtilmemiş"}\`
         • Banlayan kullanıcı: ${message.guild.members.cache.get(banData ? banData.executor : "123") ? message.guild.members.cache.get(banData.executor) : `Sunucuda değil (${banData.executor})`}
         • Ban süresi: \`${banData ? banData.type : "Perma"}\`
-        • Açılacağı tarih: \`${banData && (banData.type === "temp") ? banData.duration - checkDays(banData.created) : "Açılmayacak"}\`
-        `).setColor('BLACK').setTitle("† Dante's INFEЯИO");
-        await message.channel.send(embed);
+        • Banın Açılacağı tarih: \`${banData && (banData.type === "temp") ? banData.duration - checkDays(banData.created) : "Açılmayacak"}\`
+        `).setColor('BLACK').setTimestamp().setFooter(`• İkinici bir şans? -Kahve 🌟`).setTitle("† Dante's INFEЯИO");
+        await message.channel.send(embed).then(msg => msg.delete({ timeout: 13000 }));
         client.cmdCooldown[message.author.id][this.info.name] = Date.now() + this.info.cooldown;
     }
 }
