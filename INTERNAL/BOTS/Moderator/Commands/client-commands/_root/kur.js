1d823861a8493933aaaa8c5f51a43fd5f33a4ad6
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-27 07:14:55 +0300

    Update kur.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
@@ -36,7 +36,7 @@ class Kur extends Command {
         const myGm = Gm();
         const framePNGs = await readdir('../../../../../SRC/point_items/');
         for (let index = 0; index < framePNGs.length; index++) {
-            const frameIndex = `../../../../../SRC/point_items/${framePNGs[index]}`;
+            const frameIndex = __dirname + `/../../../../SRC/point_items/${framePNGs[index]}`;
             myGm.in(frameIndex).delay(100);
         }
         myGm.toBuffer((error, buffer) => {
