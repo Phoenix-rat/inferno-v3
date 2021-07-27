5f31dd7765b6dcfe0057556ab3b6f548e3cd6d73
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-27 09:23:19 +0300

    Update kur.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
@@ -33,17 +33,14 @@ class Kur extends Command {
         let curGm = Gm(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/0.png`).setFormat('gif');
         for (let index = 1; index <= 100; index++) {
             curGm = curGm.delay(1 * index).in(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/${index}.png`);
-            console.log(curGm);
-            if (index === 100) {
-                curGm.toBuffer((error, buffer) => {
-                    if (error) return console.log(error);
-                    const att = new Discord.MessageAttachment(buffer, 'pointBar.gif', {
-                        content_type: 'image/gif'
-                    });
-                    message.channel.send(new Discord.MessageEmbed().setImage('attachment://pointBar.gif').attachFiles(att));
-                });
-            }
         }
+        curGm.toBuffer((error, buffer) => {
+            if (error) return console.log(error);
+            const att = new Discord.MessageAttachment(buffer, 'pointBar.gif', {
+                content_type: 'image/gif'
+            });
+            message.channel.send(new Discord.MessageEmbed().setImage('attachment://pointBar.gif').attachFiles(att));
+        });
 
     }
 
