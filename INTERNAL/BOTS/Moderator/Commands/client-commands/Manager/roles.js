9fc4eb7c348b119d826d6454fb87dc52281a9ecc
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-07-28 13:30:43 +0300

    g

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Manager/roles.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Manager/roles.js
@@ -4,15 +4,14 @@ const low = require('lowdb');
 class Roles extends Command {
     constructor(client) {
         super(client, {
-            name: "roller",
-            description: "Sunucuda bulunan rolelri görüntüler",
-            usage: "roller",
-            examples: ["roller"],
-            cooldown: 3600000,
-            category: "Düzen",
-            aliases: ["roles","kroles"],
-            accaptedPerms: ["root","owner"],
-            enabled: false
+            name: "roles",
+            description: "Belirtilen roldeki üyeleri gösterir.",
+            usage: "roles @Rol/ID",
+            examples: ["roles @Rol/ID"],
+            category: "Yetkili",
+            aliases: ["roller", "groles"],
+            accaptedPerms: ["root", "owner"],
+            cooldown: 10000
         });
     }
     async run(client, message, args, data) {
