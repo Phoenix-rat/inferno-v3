351406ab12d9f08307f46c08b83352a576b2c704
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-07-28 22:00:38 +0300

    Update rolver.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Manager/rolver.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Manager/rolver.js
@@ -25,51 +25,53 @@ class Rolver extends Command {
         const gavat = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
         if (!gavat) return await message.channel.send(yetenekembed.setDescription(`Kullanıcı bulunamadı :(`))
 
-        if (!args[0] || (args[0] !== 'vip' && args[0] !== 'dj' && args[0] !== 'editör' && args[0] !== 'vokal' && args[0] !== 'ressam' && args[0] !== 'streamer' && args[0] !== 'müzisyen' && args[0] !== 'tasarımcı')) return message.channel.send('Test').then(x => x.delete({ timeout: 5000 }));
-       
-         if(args[0] === "vip") {
+        let select = args[1]
+        if (!select || (select !== 'vip' && select !== 'dj' && select !== 'editör' && select !== 'vokal' && select !== 'ressam' && select !== 'streamer' && select !== 'müzisyen' && select !== 'tasarımcı')) return message.channel.send('Test').then(x => x.delete({ timeout: 5000 }));
+
+         if(select == "vip") {
             if(!gavat.roles.cache.has(roles.get("vip").value())) {
                 gavat.roles.add(roles.get("vip").value())
                 return message.channel.send(yetenekembed.setDescription(`Başarıyla ${roles.get("vip").value()} adlı rolü verdim.`)) 
             }
         }
-        if(args[0] === "dj") {
+        if(select == "dj") {
             if(!gavat.roles.cache.has(roles.get("role_dj").value())) {
                 gavat.roles.add(roles.get("role_dj").value())
                 return message.channel.send(yetenekembed.setDescription(`Başarıyla ${roles.get("role_dj").value()} adlı rolü verdim.`)) 
             }
         }
-        if(args[0] === "editör") {
+        if(select == "editör") {
             if(!gavat.roles.cache.has(roles.get("role_editor").value())) {
                 gavat.roles.add(roles.get("role_editor").value())
                 return message.channel.send(yetenekembed.setDescription(`Başarıyla ${roles.get("role_editor").value()} adlı rolü verdim.`)) 
             }
         }
-        if(args[0] === "vokal") {
+        if(select == "vokal") {
             if(!gavat.roles.cache.has(roles.get("role_vokal").value())) {
                 gavat.roles.add(roles.get("role_vokal").value())
-                return message.channel.send(yetenekembed.setDescription(`Başarıyla ${roles.get("role_vokal").value()} adlı rolü verdim.`)) 
+                const vokalembed = new Discord.MessageEmbed().setDescription(`Başarıyla ${roles.get("role_vokal").value()} adlı rolü verdim.`)
+                return message.channel.send(vokalembed)
             }
         }
-        if(args[0] === "ressam") {
+        if(select == "ressam") {
             if(!gavat.roles.cache.has(roles.get("role_ressam").value())) {
                 gavat.roles.add(roles.get("role_ressam").value())
                 return message.channel.send(yetenekembed.setDescription(`Başarıyla ${roles.get("role_ressam").value()} adlı rolü verdim.`)) 
             }
         }
-        if(args[0] === "streamer") {
+        if(select == "streamer") {
             if(!gavat.roles.cache.has(roles.get("role_streamer").value())) {
                 gavat.roles.add(roles.get("role_streamer").value())
                 return message.channel.send(yetenekembed.setDescription(`Başarıyla ${roles.get("role_streamer").value()} adlı rolü verdim.`)) 
             }
         }
-        if(args[0] === "müzisyen") {
+        if(select == "müzisyen") {
             if(!gavat.roles.cache.has(roles.get("role_musician").value())) {
                 gavat.roles.add(roles.get("role_musician").value())
                 return message.channel.send(yetenekembed.setDescription(`Başarıyla ${roles.get("role_musician").value()} adlı rolü verdim.`)) 
             }
         }
-        if(args[0] === "tasarımcı") {
+        if(select == "tasarımcı") {
             if(!gavat.roles.cache.has(roles.get("role_designer").value())) {
                 gavat.roles.add(roles.get("role_designer").value())
                 return message.channel.send(yetenekembed.setDescription(`Başarıyla ${roles.get("role_designer").value()} adlı rolü verdim.`)) 
