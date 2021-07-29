bc7aeff0e42e85d6b8eb7a3d562864c94474646c
Stark <51216503+starktwo@users.noreply.github.com>
2021-07-29 03:21:23 +0300

    Update banner.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/General/banner.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/General/banner.js
@@ -55,7 +55,7 @@ async function avatar(client, message, member, token) {
         }
     })
     let json = await fetched.json();
-    if (json.banner == null) return message.reply("Kullanıcının BannerURLsi bulunmamaktadır.")
+    if (json.banner == null) return message.reply(`${json.username} adlı kullanıcının **Banner'i** bulunmamaktadır.`)
     let avatarGIF = `https://cdn.discordapp.com/banners/${userID}/${json.banner}.gif?size=1024`;
     let avatarPNG = `https://cdn.discordapp.com/banners/${userID}/${json.banner}.png?size=1024`;
     let avatarFetch = await fetch(avatarGIF);
