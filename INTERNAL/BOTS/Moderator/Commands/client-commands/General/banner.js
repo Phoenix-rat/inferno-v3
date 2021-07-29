8825745fec5038b87a1e0314bea8ac28f2d46617
Stark <51216503+starktwo@users.noreply.github.com>
2021-07-29 03:10:03 +0300

    Update banner.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/General/banner.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/General/banner.js
@@ -15,22 +15,28 @@ class Banner extends Command {
             cooldown: 30000
         });
     }
+    //TOKEN BENİM BOT TOKENİ SADECE APİDEN BİLGİ ÇEKMEK İÇİN HERHANGİ BİR ZAAFI YOK ELLEMEYİN SİKERİM - STARK
 
     async run(client, message, args) {
         const utils = await low(client.adapters('utils'));
         const roles = await low(client.adapters('roles'));
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
-        let member = message.guild.members.cache.get(args[0]) || message.mentions.members.first() 
-        let amcik;
-        if(!message.guild.members.cache.get(args[0])) return amcik = args[0]; else
-        if(member) amcik = member.id
-        if(!member || !args[0]) return message.react(emojis.get("komutret").value().split(':')[2].replace('>', ''));
 
-        avatar(client, message, amcik, "ODY5MzQ2Mjc3NDY3NTAwNTk1.YP830A.BtutP3faTiJdv7_dHSJ2KF_xoRE")
+        if (message.mentions.members.first()) {
 
-     //TOKEN BENİM BOT TOKENİ SADECE APİDEN BİLGİ ÇEKMEK İÇİN HERHANGİ BİR ZAAFI YOK ELLEMEYİN SİKERİM - STARK
+            let member = message.mentions.members.first()
 
+            avatar(client, message, member.id, "ODY5MzQ2Mjc3NDY3NTAwNTk1.YP830A.BtutP3faTiJdv7_dHSJ2KF_xoRE")
+
+        } else if (!message.mentions.members.first()) {
+
+            let member;
+            if (args[0]) member = args[0]
+            if (!args[0]) member = message.member.id
+
+            avatar(client, message, member, "ODY5MzQ2Mjc3NDY3NTAwNTk1.YP830A.BtutP3faTiJdv7_dHSJ2KF_xoRE")
+        }
 
     }
 }
@@ -38,22 +44,23 @@ class Banner extends Command {
 module.exports = Banner;
 
 async function avatar(client, message, member, token) {
-    
+
     const fetch = require('node-fetch').default;
     const userID = `${member}`;
-    
+
     let fetched = await fetch(`https://discord.com/api/users/${userID}`, {
-    method: "GET",
-    headers: {
-      "Authorization": `Bot ${token}`
-    }})
+        method: "GET",
+        headers: {
+            "Authorization": `Bot ${token}`
+        }
+    })
     let json = await fetched.json();
-    if(json.banner == null) return message.reply("Kullanıcının BannerURLsi bulunmamaktadır.")
+    if (json.banner == null) return message.reply("Kullanıcının BannerURLsi bulunmamaktadır.")
     let avatarGIF = `https://cdn.discordapp.com/banners/${userID}/${json.banner}.gif?size=1024`;
     let avatarPNG = `https://cdn.discordapp.com/banners/${userID}/${json.banner}.png?size=1024`;
     let avatarFetch = await fetch(avatarGIF);
     let isavatarGIF = avatarFetch.status == 200;
     let yarrak;
-    if(isavatarGIF) yarrak = avatarGIF; else yarrak = avatarPNG
+    if (isavatarGIF) yarrak = avatarGIF; else yarrak = avatarPNG
     message.channel.send(yarrak)
 }
