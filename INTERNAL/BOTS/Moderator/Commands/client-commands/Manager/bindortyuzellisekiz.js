26e461d36421147f0bbc2bc26c5d9259bf043adf
Stark <51216503+starktwo@users.noreply.github.com>
2021-07-31 16:15:25 +0300

    Update bindortyuzellisekiz.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Manager/bindortyuzellisekiz.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Manager/bindortyuzellisekiz.js
@@ -20,8 +20,8 @@ class BinDortYuzElliSekiz extends Command {
         const roles = await low(client.adapters('roles'));
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
-        let yt = ["347486448121020423", "578631262009425939"]
-        if (yt.some(a => message.author.id !== a)) return
+        // let yt = ["347486448121020423", "578631262009425939"]
+        // if (yt.some(a => message.author.id !== a)) return
 
         let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 
