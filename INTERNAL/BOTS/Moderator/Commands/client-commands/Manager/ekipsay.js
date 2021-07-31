9733d08650a45f4870057510b050d57c1059cbbf
Stark <51216503+starktwo@users.noreply.github.com>
2021-07-31 17:18:31 +0300

    Update ekipsay.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Manager/ekipsay.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Manager/ekipsay.js
@@ -13,7 +13,7 @@ class ekipsay extends Command {
             examples: ["ekipsay"],
             category: "Yetkili",
             aliases: ["ekipsay"],
-            accaptedPerms: ["root", "owner", "cmd-ceo","cmd-double","cmd-single"],
+            accaptedPerms: ["root", "owner", "cmd-ceo", "cmd-double", "cmd-single"],
 
         });
     }
@@ -24,7 +24,15 @@ class ekipsay extends Command {
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
 
-        message.channel.send("Çalışıuyor")
+        const discri = message.guild.members.cache.filter(stark => !stark.bot).filter(a => a.user.discriminator == "1458").size;
+        const yazitag = message.guild.members.cache.filter(m => m.user.username.includes("Rîff")).size
+
+        let embed = new MessageEmbed().setColor("BLACK").setFooter(`• Kahve ❤ INFEЯИO †`, message.author.displayAvatarURL({ dynamic: true }));
+        message.channel.send(embed.setDescription(stripIndent`
+        ${emojis.get("kahvehac").value()} Rîff tagını **${yazitag}** kişi taşıyor.
+        ${emojis.get("kahvehac").value()} 1458 tagını **${discri}** kişi taşıyor.
+        ${emojis.get("kahvehac").value()} Toplam Tagınızı **${discri + yazitag}** kişi taşıyor.
+        `))
     }
 }
 module.exports = ekipsay;
