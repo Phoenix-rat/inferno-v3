a1b7afeb3251ff7d03faa50605ceb4436e599ebe
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-31 21:41:05 +0300

    Update tstat.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Stat/tstat.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Stat/tstat.js
@@ -20,7 +20,7 @@ class Invites extends Command {
             usage: "tstat ses/davet/teyit",
             examples: ["tstat ses/davet/teyit"],
             category: "Stats",
-            aliases: ["t31","me"],
+            aliases: ["t31", "me"],
             accaptedPerms: ["crew"],
             cooldown: 10000
         })
@@ -31,7 +31,18 @@ class Invites extends Command {
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
         const mentioned = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+        function msToTime(duration) {
+            var milliseconds = Math.floor((duration % 1000) / 100),
+                seconds = Math.floor((duration / 1000) % 60),
+                minutes = Math.floor((duration / (1000 * 60)) % 60),
+                hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
+            hours = (hours < 10) ? "0" + hours : hours;
+            minutes = (minutes < 10) ? "0" + minutes : minutes;
+            seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+            return hours + "Saat, " + minutes + "Dakika, " + seconds + "Saniye";
+        }
         let tstatstatus = mentioned.presence.status
             .replace('online', 'Çevrim İçi <:inferno_cervimici:866719561944662016>')
             .replace('idle', 'Boşta <:inferno_bostaa:866719581493526549>')
@@ -62,17 +73,17 @@ class Invites extends Command {
             • Kullanıcı: ${mentioned}
             • Durum: ${tstatstatus}
             • Sunucuya Katılma Tarihi: \`${moment(mentioned.joinedAt).format("LLL")}\`
-            • Geçirilen toplam süre: \`${new Date(records.map(r => r.duration).reduce((a, b) => a + b, 0)).toISOString().substr(11, 8).toString().split(':').map((v, i) => `${v} ${birim[i]}`).join(' ')}\`
+            • Geçirilen toplam süre: \`${msToTime(records.map(r => r.duration).reduce((a, b) => a + b, 0))}\`
 
             **Ses Bilgileri:**
-            • Public ses süresi: \`${new Date(records.filter(r => r.channelType === "st_public").map(r => r.duration).reduce((a, b) => a + b, 0)).toISOString().substr(11, 8).toString().split(':').map((v, i) => `${v} ${birim[i]}`).join(' ')}\`
-            • Register ses süresi: \`${new Date(records.filter(r => r.channelType === "st_registry").map(r => r.duration).reduce((a, b) => a + b, 0)).toISOString().substr(11, 8).toString().split(':').map((v, i) => `${v} ${birim[i]}`).join(' ')}\`
-            • Private ses süsresi: \`${new Date(records.filter(r => r.channelType === "st_private").map(r => r.duration).reduce((a, b) => a + b, 0)).toISOString().substr(11, 8).toString().split(':').map((v, i) => `${v} ${birim[i]}`).join(' ')}\`
+            • Public ses süresi: \`${msToTime(records.filter(r => r.channelType === "st_public").map(r => r.duration).reduce((a, b) => a + b, 0))}\`
+            • Register ses süresi: \`${msToTime(records.filter(r => r.channelType === "st_registry").map(r => r.duration).reduce((a, b) => a + b, 0))}\`
+            • Private ses süresi: \`${msToTime(records.filter(r => r.channelType === "st_private").map(r => r.duration).reduce((a, b) => a + b, 0))}\`
 
             **Toplam Ses İstatistikleri**
-            • Toplam ses: \`${new Date(records.map(r => r.duration).reduce((a, b) => a + b, 0)).toISOString().substr(11, 8).toString().split(':').map((v, i) => `${v} ${birim[i]}`).join(' ')}\`
-            • Mikrofon kapalı: \`${new Date(records.filter(r => r.selfMute).map(r => r.duration).reduce((a, b) => a + b, 0)).toISOString().substr(11, 8).toString().split(':').map((v, i) => `${v} ${birim[i]}`).join(' ')}\`
-            • Kulaklık kapalı: \`${new Date(records.filter(r => r.selfMute).map(r => r.duration).reduce((a, b) => a + b, 0)).toISOString().substr(11, 8).toString().split(':').map((v, i) => `${v} ${birim[i]}`).join(' ')}\`
+            • Toplam ses: \`${msToTime(records.map(r => r.duration).reduce((a, b) => a + b, 0))}\`
+            • Mikrofon kapalı: \`${msToTime(records.filter(r => r.selfMute).map(r => r.duration).reduce((a, b) => a + b, 0))}\`
+            • Kulaklık kapalı: \`${msToTime(records.filter(r => r.selfMute).map(r => r.duration).reduce((a, b) => a + b, 0))}\`
          `).setThumbnail(mentioned.user.displayAvatarURL({ dynamic: true })).setColor(mentioned.displayHexColor).setFooter("• Kahve seni önemsiyor- vallaha önemsiyom abi").setTitle(message.guild.name);
             return await message.channel.send(responseEmbed).then(msg => msg.delete({ timeout: 20000 }));
         }
@@ -99,7 +110,7 @@ class Invites extends Command {
             `).setThumbnail(mentioned.user.displayAvatarURL({ dynamic: true })).setColor(mentioned.displayHexColor).setTitle(message.guild.name);
             return await message.channel.send(embedD).then(msg => msg.delete({ timeout: 10000 }));
         }
-        if(args[0] == 'mesaj') {
+        if (args[0] == 'mesaj') {
             const Data = await stat_msg.findOne({ _id: mentioned.user.id });
             if (!Data) return message.channel.send(`${emojis.get("kullaniciyok").value()} Data bulunamadı.`);
             const records = Data.records.filter(r => checkDays(r.created) < days);
@@ -110,7 +121,7 @@ class Invites extends Command {
                 stats[element.channel] = stats[element.channel] + 1;
             }
             const description = Object.keys(stats).map(channelID => `\`•\` ${message.guild.channels.cache.get(channelID) || "\`Bilinmiyor\`"}: ${stats[channelID] || 0} mesaj`).join('\n');
-                        
+
             const responseEmbed = new Discord.MessageEmbed().setDescription(stripIndent`
             ${mentioned} kişisine ait ${days} günlük mesaj bilgileri:
         
