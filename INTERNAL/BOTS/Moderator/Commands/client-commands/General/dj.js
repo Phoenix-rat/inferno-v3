8f0c4a729d6b0067aa66922c279f7981b1c6e78a
Stark <51216503+starktwo@users.noreply.github.com>
2021-08-01 22:24:17 +0300

    Update dj.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/General/dj.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/General/dj.js
@@ -23,18 +23,18 @@ class Dj extends Command {
         const channels = await low(client.adapters('channels'));
 
         const roleID = roles.get("role_dj").value();
-        const myRole = message.guild.roles.cache.get(roleID);
+        const myRole = message.guild.roles.cache.get("857424034349842483");
         const embed = new Discord.MessageEmbed().setColor("RANDOM")
         const mentioned = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         
         if (!mentioned) return message.channel.send(new Discord.MessageEmbed()
         .setDescription(`${emojis.get("kullaniciyok").value()} Kişiyi bulunamadım.`).setColor('RANDOM'));
-        if (!mentioned.roles.cache.has(roleID)) {
+        if (!mentioned.roles.cache.has("857424034349842483")) {
             await message.channel.send(embed
                 .setDescription(`${mentioned} kişisine **${myRole.name}** adlı rolü başarıyla verdim!`));
-            await mentioned.roles.add(myRole.id);
+            await mentioned.roles.add("857424034349842483");
         } else {
-            await mentioned.roles.remove(myRole.id);
+            await mentioned.roles.remove("857424034349842483");
             await message.channel.send(embed
             .setDescription(`${mentioned} kişisinden **${myRole.name}** adlı rolü başarıyla aldım!`));
         }
