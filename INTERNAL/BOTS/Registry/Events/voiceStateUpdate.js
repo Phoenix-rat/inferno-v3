d9303784a6f69271d05723932d1b5d7099a596ac
Stark <51216503+starktwo@users.noreply.github.com>
2021-08-02 11:23:47 +0300

    Update voiceStateUpdate.js

--- INTERNAL/BOTS/Registry/Events/voiceStateUpdate.js
+++ INTERNAL/BOTS/Registry/Events/voiceStateUpdate.js
@@ -16,7 +16,7 @@ class VoiceStateUpdate {
         const privChannels = await private_channels.find();
         const channel = client.guild.channels.cache.get(channels.get("oda_olustur").value());
         if (prev.channel && privChannels.some(c => c._id === prev.channel.id)) {
-            if (prev.channel.members.cache.filter(a => !a.user.bot && a.channel.members.size === 0)) {
+            if (prev.voice.channel.members.cache.filter(a => !a.user.bot).size < 1) {
                 await private_channels.deleteOne({ _id: prev.channel.id });
                 await prev.channel.delete();
                 return;
