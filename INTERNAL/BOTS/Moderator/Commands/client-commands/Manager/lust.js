fdfd6e65f7692abfcdcf5eaca27a794ce9882293
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-08-03 01:51:24 +0300

    Update lust.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Manager/lust.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Manager/lust.js
@@ -23,9 +23,9 @@ class Crax extends Command {
 
         let member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
         if (!member) return message.react(emojis.get("warn").value().split(':')[2].replace('>', ''));
-        if (member.roles.cache.has("871446895665491978")) return message.reply("Kullanıcıda zaten perm var bilader amacın ne ?").catch(() => { })
+        if (member.roles.cache.has(roles.get("starter").value())) return message.reply("Kullanıcıda zaten perm var bilader amacın ne ?").catch(() => { })
 
-        member.roles.add("871446895665491978").catch(() => { })
+        member.roles.add(roles.get("starter").value()).catch(() => { })
         message.react(emojis.get("ok").value().split(':')[2].replace('>', ''));
     }
 }
