6decb0628bfb8da4ae660ef7b677907c4345ed32
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-08-04 22:03:34 +0300

    c

--- INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
@@ -7,10 +7,7 @@ const fs = require('fs');
 const Gm = require('gm');
 const request = require('request');
 
-const pngCanvases = [];
-for
-
-
+let templateImage = null;
 
 class Kur extends Command {
 
@@ -32,73 +29,81 @@ class Kur extends Command {
             dmCmd: false
         });
     }
-
+    /**
+     * @param {import("discord.js").Message} message
+    */
     async run(client, message, args, data) {
         const utils = await low(client.adapters('utils'));
         const roles = await low(client.adapters('roles'));
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
         
-
-
-
-
-        const encoder = new GIFEncoder(1000, 400);
-        encoder.start();
-        encoder.setRepeat(-1);   // 0 for repeat, -1 for no-repeat
-        encoder.setDelay(1);  // frame delay in ms
-        encoder.setQuality(10); // image quality. 10 is default.
-        // use node-canvas
-        const canvas = Canvas.createCanvas(1000, 400);
-        const context = canvas.getContext('2d');
-        const pngFiles = fs.readdirSync(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/`).map(str => str.split('.')[0].slice(2)).sort((a, b) => Number(a) - Number(b));
-        for (let index = 0; index < (args[0] ? Math.round(Number(args[2]) / 4) : pngFiles.length); index++) {
-            let file;
-            try {
-                file = fs.open(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/1-${pngFiles[index]}.png`, 'r', (error, fd) => {
-                    if (error) index = index + 1;
-                });
-            } finally {
-                if (file) await file.close();
-            }
-            const background = await Canvas.loadImage(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/1-${pngFiles[index]}.png`);
-            context.drawImage(background, 0, 0, 1000, 400);
-
-            request(message.author.displayAvatarURL({ format: 'gif' }), {
-                encoding: null
-            }, async (error, response, body) => {
-                gifFrames({
-                    url: message.author.displayAvatarURL({ format: 'gif' }),
-                    frames: 0,
-                    outputType: 'canvas'
-                }).then((frameData) => {
-                    console.log(frameData[index]);
-                });
-                //await message.channel.send(new Discord.MessageAttachment(body, 'body.gif'));
-                /*
-                frameGm.toBuffer(async (err, buffer) => {
-                    if (err) return console.log(err);
-                    await message.channel.send(new Discord.MessageAttachment(buffer, 'points.png'));
-                    const myGm = Gm(buffer).in().resize(200).selectFrame(index).setFormat('png');
-                    myGm.toBuffer(async (er, buff) => {
-                        console.log(buff);
-                        if (er) return console.log(er);
-                        await message.channel.send(new Discord.MessageAttachment(buff, 'buffer.png'));
-                        const avatar = await Canvas.loadImage(buff);
-                        context.drawImage(avatar, 75, 60, 200, 200);
-                    });
-                });
-                */
-            });
-
-            encoder.addFrame(context);
-            console.log(index);
+        if (!templateImage) {
+            templateImage = await Canvas.loadImage(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/template.png`);
         }
-        encoder.finish();
-        console.log('done!');
-        const ender = encoder.out.getData();
-        const attachment = new Discord.MessageAttachment(ender, 'my-points.gif');
-        await message.channel.send(attachment);
+
+        let canvas = Canvas.createCanvas(1000, 400)
+        let ctx = canvas.getContext("2d");
+
+        
+        
+
+        // const encoder = new GIFEncoder(1000, 400);
+        // encoder.start();
+        // encoder.setRepeat(-1);   // 0 for repeat, -1 for no-repeat
+        // encoder.setDelay(1);  // frame delay in ms
+        // encoder.setQuality(10); // image quality. 10 is default.
+        // // use node-canvas
+        // const canvas = Canvas.createCanvas(1000, 400);
+        // const context = canvas.getContext('2d');
+        // const pngFiles = fs.readdirSync(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/`).map(str => str.split('.')[0].slice(2)).sort((a, b) => Number(a) - Number(b));
+        // for (let index = 0; index < (args[0] ? Math.round(Number(args[2]) / 4) : pngFiles.length); index++) {
+        //     let file;
+        //     try {
+        //         file = fs.open(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/1-${pngFiles[index]}.png`, 'r', (error, fd) => {
+        //             if (error) index = index + 1;
+        //         });
+        //     } finally {
+        //         if (file) await file.close();
+        //     }
+        //     const background = await Canvas.loadImage(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/1-${pngFiles[index]}.png`);
+        //     context.drawImage(background, 0, 0, 1000, 400);
+
+        //     request(message.author.displayAvatarURL({ format: 'gif' }), {
+        //         encoding: null
+        //     }, async (error, response, body) => {
+        //         gifFrames({
+        //             url: message.author.displayAvatarURL({ format: 'gif' }),
+        //             frames: 0,
+        //             outputType: 'canvas'
+        //         }).then((frameData) => {
+        //             console.log(frameData[index]);
+        //         });
+        //         //await message.channel.send(new Discord.MessageAttachment(body, 'body.gif'));
+        //         /*
+        //         frameGm.toBuffer(async (err, buffer) => {
+        //             if (err) return console.log(err);
+        //             await message.channel.send(new Discord.MessageAttachment(buffer, 'points.png'));
+        //             const myGm = Gm(buffer).in().resize(200).selectFrame(index).setFormat('png');
+        //             myGm.toBuffer(async (er, buff) => {
+        //                 console.log(buff);
+        //                 if (er) return console.log(er);
+        //                 await message.channel.send(new Discord.MessageAttachment(buff, 'buffer.png'));
+        //                 const avatar = await Canvas.loadImage(buff);
+        //                 context.drawImage(avatar, 75, 60, 200, 200);
+        //             });
+        //         });
+        //         */
+        //     });
+
+        //     encoder.addFrame(context);
+        //     console.log(index);
+        // }
+        // encoder.finish();
+        // console.log('done!');
+        // const ender = encoder.out.getData();
+        // const attachment = new Discord.MessageAttachment(ender, 'my-points.gif');
+        // await message.channel.send(attachment);
 
         
 
