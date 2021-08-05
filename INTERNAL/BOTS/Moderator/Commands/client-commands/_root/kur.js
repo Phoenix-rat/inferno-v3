639aaf29e919f2b3bcbe6b80574892d9f8787df9
Stark <51216503+starktwo@users.noreply.github.com>
2021-08-05 12:10:47 +0300

    +

--- INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/_root/kur.js
@@ -2,110 +2,156 @@ const Command = require("../../../Base/Command");
 const low = require('lowdb');
 const Discord = require('discord.js');
 const Canvas = require('canvas');
-const GIFEncoder = require('gifencoder');
 const fs = require('fs');
-const Gm = require('gm');
-const request = require('request');
-
-let templateImage = null;
-
+const path = require('path');
+const percent = require("stuffs/lib/percent");
+const makeSureFolderExists = require("stuffs/lib/makeSureFolderExists");
+const randomString = require("stuffs/lib/randomString");
+const execAsync = require("util").promisify(require("child_process").exec);
+const os = require("os");
+const Roles = require("../../../../../BASE/yetkiroles.json")
 class Kur extends Command {
 
     constructor(client) {
         super(client, {
-            name: "kur",
+            name: "newstark",
             description: "Açıklama Belirtilmemiş.",
             usage: "Kullanım Belirtilmemiş.",
             examples: ["Örnek Bulunmamakta"],
             category: "OWNER",
-            aliases: ["deneme"],
+            aliases: ["newstark"],
             acceptedRoles: [],
             cooldown: 5000,
             enabled: true,
             adminOnly: false,
             ownerOnly: false,
-            onTest: false,
-            rootOnly: true,
+            onTest: true,
+            rootOnly: false,
             dmCmd: false
         });
     }
     /**
      * @param {import("discord.js").Message} message
+     * @type {Canvas.Image} 
     */
     async run(client, message, args, data) {
         const utils = await low(client.adapters('utils'));
         const roles = await low(client.adapters('roles'));
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
-        
+
+        let member = message.guild.members.cache.get(args[0]) || message.mentions.members.first()
+        const avatarURL = memeber.user.avatarURL({ format: "png" })
+        let templateImage = null;
+        let templateImagePath = "../../../../../SRC/point_items/template.png"
+        let yetkiNumber;
+        let sahipOlunanRol = Number();
+        for (yetkiNumber = 0; yetkiNumber < Roles.Roles.length; yetkiNumber++) {
+            if (member.roles.cache.has(Roles.Roles[yetkiNumber])) {
+                sahipOlunanRol += yetkiNumber
+            };
+        }
+        const roleText = `${message.guild.roles.cache.get(sahipOlunanRol).name}`;
+        const roleColor = `${message.guild.roles.cache.get(sahipOlunanRol).hexColor}`;
+        const sesText = "600 Saat";
+        const msgText = "1000 Mesaj";
+        const usernameText = `${member.displayName ? member.displayName : member.user.username}`
+        const xpCurrent = 100;
+        const xpMax = 100;
+        const xpBarColor = "#4200ff";
+        const badgePaths = Array(5).fill("").map((_, i) => `../../../../../SRC/badges/badge${i + 1}.png`);
+
         if (!templateImage) {
-            templateImage = await Canvas.loadImage(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/template.png`);
+            templateImage = await Canvas.loadImage(templateImagePath);
         }
 
-        let canvas = Canvas.createCanvas(1000, 400)
+        let tmpFolder = path.resolve(`../../../../../SRC/tmp/profile/${randomString(8)}`);
+        await makeSureFolderExists(tmpFolder);
+
+        let canvas = Canvas.createCanvas(500, 200);
         let ctx = canvas.getContext("2d");
 
-        
-        
+        ctx.textAlign = "left";
+        ctx.textBaseline = "top";
 
-        // const encoder = new GIFEncoder(1000, 400);
-        // encoder.start();
-        // encoder.setRepeat(-1);   // 0 for repeat, -1 for no-repeat
-        // encoder.setDelay(1);  // frame delay in ms
-        // encoder.setQuality(10); // image quality. 10 is default.
-        // // use node-canvas
-        // const canvas = Canvas.createCanvas(1000, 400);
-        // const context = canvas.getContext('2d');
-        // const pngFiles = fs.readdirSync(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/`).map(str => str.split('.')[0].slice(2)).sort((a, b) => Number(a) - Number(b));
-        // for (let index = 0; index < (args[0] ? Math.round(Number(args[2]) / 4) : pngFiles.length); index++) {
-        //     let file;
-        //     try {
-        //         file = fs.open(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/1-${pngFiles[index]}.png`, 'r', (error, fd) => {
-        //             if (error) index = index + 1;
-        //         });
-        //     } finally {
-        //         if (file) await file.close();
-        //     }
-        //     const background = await Canvas.loadImage(`/home/inferno/inferno-v3/INTERNAL/SRC/point_items/1-${pngFiles[index]}.png`);
-        //     context.drawImage(background, 0, 0, 1000, 400);
+        // Draw Background
+        ctx.drawImage(templateImage, 0, 0);
 
-        //     request(message.author.displayAvatarURL({ format: 'gif' }), {
-        //         encoding: null
-        //     }, async (error, response, body) => {
-        //         gifFrames({
-        //             url: message.author.displayAvatarURL({ format: 'gif' }),
-        //             frames: 0,
-        //             outputType: 'canvas'
-        //         }).then((frameData) => {
-        //             console.log(frameData[index]);
-        //         });
-        //         //await message.channel.send(new Discord.MessageAttachment(body, 'body.gif'));
-        //         /*
-        //         frameGm.toBuffer(async (err, buffer) => {
-        //             if (err) return console.log(err);
-        //             await message.channel.send(new Discord.MessageAttachment(buffer, 'points.png'));
-        //             const myGm = Gm(buffer).in().resize(200).selectFrame(index).setFormat('png');
-        //             myGm.toBuffer(async (er, buff) => {
-        //                 console.log(buff);
-        //                 if (er) return console.log(er);
-        //                 await message.channel.send(new Discord.MessageAttachment(buff, 'buffer.png'));
-        //                 const avatar = await Canvas.loadImage(buff);
-        //                 context.drawImage(avatar, 75, 60, 200, 200);
-        //             });
-        //         });
-        //         */
-        //     });
+        // Draw Avatar
+        ctx.drawImage(await Canvas.loadImage(avatarURL), 26, 14, 125, 125);
 
-        //     encoder.addFrame(context);
-        //     console.log(index);
-        // }
-        // encoder.finish();
-        // console.log('done!');
-        // const ender = encoder.out.getData();
-        // const attachment = new Discord.MessageAttachment(ender, 'my-points.gif');
-        // await message.channel.send(attachment);
+        ctx.font = "600 12px Arial";
 
-        
+        // Draw Role
+        const { width: roleTextWidth, emHeightDescent: roleTextHeight } = ctx.measureText(roleText);
+        const roleTextBgPadding = 4;
+
+        ctx.fillStyle = `${roleColor}55`;
+        ctx.fillRect(87 - (roleTextWidth / 2) - roleTextBgPadding, 166 - roleTextBgPadding, roleTextWidth + roleTextBgPadding * 2, roleTextHeight + roleTextBgPadding * 2)
+
+        ctx.fillStyle = "whitesmoke";
+        ctx.fillText(roleText, 87 - (roleTextWidth / 2), 166);
+
+        // Draw Ses Text
+        ctx.font = "bold 12px Arial";
+
+        const { width: sesTextWidth } = ctx.measureText(sesText);
+        ctx.fillText(sesText, 416 - (sesTextWidth / 2), 85);
+
+        // Draw Msg Text
+        const { width: msgTextWidth } = ctx.measureText(msgText);
+        ctx.fillText(msgText, 416 - (msgTextWidth / 2), 121);
+
+        // Draw Username Text
+        ctx.font = "bold 14px Arial";
+        ctx.fillText(usernameText, 205, 31, 134);
+
+        let frameIndex = 0;
+
+        // Draw Badges
+        for (let i = 0; i < badgePaths.length; i++) {
+            const badgePath = badgePaths[i];
+            frameIndex += 1;
+
+            ctx.drawImage(await Canvas.loadImage(badgePath), 172 + (i * 26), 90);
+
+            let framePath = path.resolve(tmpFolder, `frame${frameIndex.toString().padStart(3, "0")}.png`);
+            await fs.promises.writeFile(framePath, canvas.toBuffer());
+        }
+
+        // Draw Bar Animation
+        let progressTotal = percent(xpCurrent, xpMax, 300, 1);
+        let progressX = 171;
+        let progressY = 162;
+        let progressSteps = 10;
+        let progressOneStepSize = progressTotal / progressSteps;
+
+        ctx.fillStyle = xpBarColor;
+
+        for (let i = 0; i < progressSteps; i++) {
+            frameIndex += 1;
+            ctx.fillRect(progressX, progressY, progressOneStepSize * (i + 1), 20);
+            let framePath = path.resolve(tmpFolder, `frame${frameIndex.toString().padStart(3, "0")}.png`);
+            await fs.promises.writeFile(framePath, canvas.toBuffer());
+        }
+
+
+        // Generating Color Platte For Better Output
+        await execAsync(`ffmpeg -i "frame%003d.png" -vf palettegen palette.png -y`, { timeout: 5000, cwd: tmpFolder });
+
+        // Encoding the gif.
+        let outputGifPath = path.resolve(tmpFolder, "output.gif");
+        await execAsync(`ffmpeg -i "frame%003d.png" -i "palette.png" -loop -1 -filter_complex "[0]setpts=4*PTS[slowed];[slowed]paletteuse" "${outputGifPath}" -y`, { timeout: 5000, cwd: tmpFolder });
+
+        // SEND TO DISCORD
+
+      let adana = fs.copyFileSync(outputGifPath, "output.gif");
+      const attachment = new Discord.MessageAttachment(adana);
+      message.channel.send({files: [attachment]})
+
+        // AFTER SEND STUFF
+        // Deleting temp files..
+        await execAsync(os.platform() == "win32" ? `rmdir /S /Q "${tmpFolder}"` : `rm -rf "${tmpFolder}"`, { timeout: 5000 });
 
     }
 
