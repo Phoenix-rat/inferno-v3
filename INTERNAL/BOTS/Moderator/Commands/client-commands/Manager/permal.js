dd1f702c7fe120a36f606b00b2e5c31045c65296
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-08-06 01:16:08 +0300

    Create permal.js

---
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Manager/permal.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const Command = require("../../../Base/Command");
+const low = require('lowdb');
+class Permal extends Command {
+
+    constructor(client) {
+        super(client, {
+            name: "permal",
+            description: "Belirtilen kullanıcının yetkisini alçaltır",
+            usage: "permal @etiket/id",
+            examples: ["permal 674565119161794560"],
+            category: "Management",
+            accaptedPerms: ["root", "owner", "cmd-ceo", "cmd-double", "cmd-single"],
+        });
+    }
+
+    async run(client, message, args) {
+        const utils = await low(client.adapters('utils'));
+        const roles = await low(client.adapters('roles'));
+        const emojis = await low(client.adapters('emojis'));
+        const channels = await low(client.adapters('channels'));
+
+        const mentioned = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        if (!mentioned) return message.channel.send(new Discord.MessageEmbed().setDescription(`${emojis.get("kullaniciyok").value()} Kullanıcı bulunamadı!`).setColor('BLACK'));
+
+        message.channel.send(new Discord.MessageEmbed().setColor("BLACK").setDescription(`${emojis.get("kullaniciyok").value()} Belirtilen yetkiliyi yetkiden almak istediğine eminmisin?`)).then(async msj => {
+            await msj.react('✅');
+            const kabul = (reaction, user) => {
+                return ['✅'].includes(reaction.emoji.name) && user.id === message.author.id;
+            };
+            msj.awaitReactions(kabul, { max: 1, time: 50000, error: ['time'] }).then(async c => {
+                let cevap = c.first();
+                if (cevap) {
+                    if (mentioned.roles.cache.has("854162987619057665")) await mentioned.roles.set(["854162987619057665"]).catch(e => { })
+                    if (mentioned.roles.cache.has("854162990534623233")) await mentioned.roles.set(["854162990534623233"]).catch(e => { })
+                    if (mentioned.user.username.includes("†")) await mentioned.roles.add("855651068591341588").catch(e => { })
+                    await msj.delete().catch(e => { });
+                }
+            })
+        });
+    }
+}
+
+module.exports = Permal;
