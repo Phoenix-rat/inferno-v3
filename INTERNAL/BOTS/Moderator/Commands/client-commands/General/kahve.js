2bccec956400ba65ba39106296c401772ca1365a
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-09-08 23:22:16 +0300

    h

--- INTERNAL/BOTS/Moderator/Commands/client-commands/General/kahve.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/General/kahve.js
@@ -3,7 +3,9 @@ const Command = require("../../../Base/Command");
 const low = require('lowdb');
 const { stripIndent } = require('common-tags');
 const Messages = require('../../../../../MODELS/StatUses/stat_msg');
-const Register = require('../../../../../MODELS/Datalake/Registered');
+const Registers = require('../../../../../MODELS/Datalake/Registered');
+const InvitesData = require('../../../../../MODELS/StatUses/Invites');
+const Invites = require('../../../../../MODELS/StatUses/Invites');
 
 class Nerede extends Command {
 
@@ -27,15 +29,17 @@ class Nerede extends Command {
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
 
-
         const mentioned = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
         
         const Veri = await Messages.findOne({ _id: mentioned.user.id });
-        const MesajVeri = Veri ? Veri.records.length : "Veri Bulunamadı";
+        const MesajVeri = Veri ? Veri.records.length + "Mesaj" : "Veri Bulunamadı";
 
-        const TVeri = await Register.find({ executor: mentioned.user.id });
-        const KayıtVeri = TVeri ? TVeri.length : "Veri Bulunamadı";
+        const TVeri = await Registers.findOne({ executor: mentioned.user.id });
+        const KayıtVeri = TVeri ? TVeri.length + "Kayıt" : "Veri Bulunamadı";
 
+        const DVeri = await Invites.findOne({ _id: mentioned.user.id });
+        const DavetVeri = DVeri ? DVeri.records.length + "Davet" : "Veri Bulunamadı";
+  
         const MyRole = message.guild.roles.cache.get("856266299285045288");
         const NextRole = message.guild.roles.cache.get("856265230187102259")
         
@@ -43,12 +47,14 @@ class Nerede extends Command {
         .addField("__**Toplam Ses**__", `\`\`\`fix\nVeri Bulunamadı\`\`\``, true)
         .addField("__**Toplam Mesaj**__", `\`\`\`fix\n${MesajVeri}\`\`\``, true)
         .addField("__**Toplam Kayıt**__", `\`\`\`fix\n${KayıtVeri}\`\`\``, true)
-        .addField("__**Toplam Davet**__", `\`\`\`fix\nVeri Bulunamadı\`\`\``, true)
+        .addField("__**Toplam Davet**__", `\`\`\`fix\n${DavetVeri}\`\`\``, true)
         .addField("__**Toplam Taglı**__", `\`\`\`fix\nVeri Bulunamadı\`\`\``, true)
         .addField("__**Toplam Yetkili**__", `\`\`\`fix\nVeri Bulunamadı\`\`\``, true)
-        .addField(`Ses Kanalları`,`${emojis.get("statssh").value()} **Public Ses Kanalları:** \`31 saat, 31 dakika\`
-        ${emojis.get("statssh").value()} **Kayıt Ses Kanalları:** \`31 saat, 31 dakika\``)
-        .addField(`Mesaj Kanalları`,`${emojis.get("statssh").value()} **Mesaj Kanalları:** \`31 mesaj\``)
+        .addField(`Ses Kanalları`,`${emojis.get("statssh").value()} **Sohbet Odaları:** \`31 saat, 31 dakika\`
+        ${emojis.get("statssh").value()} **Kayıt Odaları:** \`31 saat, 31 dakika\`
+        ${emojis.get("statssh").value()} **Private Odaları:** \`31 saat, 31 dakika\`
+        ${emojis.get("statssh").value()} **Eğlence Odaları:** \`31 saat, 31 dakika\``)
+        .addField(`Mesaj Kanalları`,`${emojis.get("statssh").value()} **Mesaj Kanalları:** \`${MesajVeri}\``)
         .addField(`${emojis.get("statstars").value()} Puan Durumu`,`${bar(10000, 25000)} \`10000/25000\``)
         .addField(`${emojis.get("statstars").value()} Yetki Atlama Durumu`,`${MyRole} rolünden ${NextRole} rolüne yükselmek için \`15000\` **Puana** ihtiyacın var!`)
 
