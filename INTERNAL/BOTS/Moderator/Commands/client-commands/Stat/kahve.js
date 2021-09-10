eb203031770083d34738354463acbd1963b60c91
Kahverella <82963521+Kahverella@users.noreply.github.com>
2021-09-10 13:22:58 +0300

    Update kahve.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Stat/kahve.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Stat/kahve.js
@@ -5,7 +5,6 @@ const { stripIndent } = require('common-tags');
 const Messages = require('../../../../../MODELS/StatUses/stat_msg');
 const Register = require('../../../../../MODELS/Datalake/Registered');
 const Invites = require('../../../../../MODELS/StatUses/Invites');
-const { bar } = require('../../../../../HELPERS/functions')
 
 class Nerede extends Command {
     constructor(client) {
@@ -58,6 +57,25 @@ class Nerede extends Command {
 
         await message.channel.send(embed)
 
+        function bar(point, maxPoint) {
+            const deger = Math.trunc(point * 10 / maxPoint);
+            let str = "";
+            for (let index = 2; index < 9; index++) {
+                if ((deger / index) >= 1) {
+                    str = str + emojis.get("ortabar_dolu").value()
+                } else {
+                    str = str + emojis.get("ortabar").value()
+                }
+            }
+            if (deger === 0) {
+                str = `${emojis.get("solbar").value()}${str}${emojis.get("sagbar").value()}`
+            } else if (deger === 10) {
+                str = `${emojis.get("solbar_dolu").value()}${str}${emojis.get("sagbar_dolu").value()}`
+            } else {
+                str = `${emojis.get("solbar_dolu").value()}${str}${emojis.get("sagbar").value()}`
+            }
+            return str;
+        }
         
     }
 }
