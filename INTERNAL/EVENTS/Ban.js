2d54196282bc3a3a807813235a5f09dfe1c5c172
Taurusian Tantoony <tantoony.id@gmail.com>
2021-09-21 02:00:38 +0300

    aa

--- INTERNAL/EVENTS/Ban.js
+++ INTERNAL/EVENTS/Ban.js
@@ -1,5 +1,7 @@
 const BanS = require('../MODELS/Moderation/Ban');
 const low = require('lowdb');
+const Discord = require('discord.js');
+const { stripIndents } = require('common-tags');
 
 class PermaBanEvent {
     constructor(client) {
@@ -26,7 +28,13 @@ class PermaBanEvent {
             await pban.save();
         }
         client.extention.emit('Record', user.id, executor, reason, "Ban", type, duration);
-
+        const embed = new Discord.MessageEmbed().setDescription(stripIndents`
+        **Banlayan:** ${guild.members.cache.get(executor)} (\`${executor}\`)
+        **Banlanan:** ${user} (\`${user.id}\`)
+        **Sebep:** ${reason || "Yok"}
+        **Süresi:** ${type === "perma" ? "Sınırsız" : `${duration} gün`}
+        `);
+        await guild.channels.cache.get(channels.get("log_ban").value()).send(embed);
     }
 }
 module.exports = PermaBanEvent;
