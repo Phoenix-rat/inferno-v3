7e34cd0865dacbb3fd8171274f5361a2a5ed1251
Taurusian Tantoony <tantoony.id@gmail.com>
2021-09-20 12:47:53 +0300

    Update baslat.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/_root/baslat.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/_root/baslat.js
@@ -6,6 +6,7 @@ const RoleData = require('../../../../../MODELS/Datalake/Roles');
 const channelData = require('../../../../../MODELS/Datalake/CatChannels');
 const txtCnl = require('../../../../../MODELS/Datalake/TextChannels');
 const vcCnl = require('../../../../../MODELS/Datalake/VoiceChannels');
+const oData = require('../../../../../MODELS/Datalake/Overwrites');
 const keyz = require('shortid');
 class Kur extends Command {
 
@@ -73,33 +74,84 @@ class Kur extends Command {
             index = index + 1;
         }, 500);
         */
-        const rawData = await channelData.find();
-        const sortedData = rawData.sort((a, b) => a.position - b.position);
-        console.log(sortedData);
-        let index = 1;
+        /*
+         const rawData = await channelData.find();
+         const sortedData = rawData.sort((a, b) => a.position - b.position);
+         console.log(sortedData);
+         let index = 1;
+         setInterval(async () => {
+             const theCnl = sortedData[index];
+             const cat = await message.guild.channels.create(theCnl.name, {
+                 type: 'category'
+             });
+             const TxtChildren = await txtCnl.find({ parentID: theCnl._id });
+             const txts = TxtChildren.sort((a, b) => a.position - b.position);
+             txts.forEach(async (c) => {
+                 await message.guild.channels.create(c.name, {
+                     type: 'text',
+                     parent: cat.id
+                 });
+             });
+             const vcChildren = await vcCnl.find({ parentID: theCnl._id });
+             const vcs = vcChildren.sort((a, b) => a.position - b.position);
+             vcs.forEach(async (c) => {
+                 await message.guild.channels.create(c.name, {
+                     type: 'voice',
+                     parent: cat.id
+                 });
+             });
+             index = index + 1;
+         }, 5000);
+         */
+        // const rawOver = await oData.find();
+        // const catChannelData = await channelData.find();
+        const myRoles = await RoleData.find();
+        // const textChannelData = await txtCnl.find();
+        // const voiceChannelData = await vcCnl.find();
+
+        function ohYeah(idm) {
+            const theDataRole = myRoles.find(rd => rd.name === message.guild.roles.cache.get(idm).name);
+            return theDataRole._id;
+        }
+
+        function fuckYeah(idm) {
+            const queSexi = message.guild.roles.cache.find(role => role.name === myRoles.find(r => r._id === idm).name);
+            return queSexi.id;
+        }
+
+        let x = 0;
         setInterval(async () => {
-            const theCnl = sortedData[index];
-            const cat = await message.guild.channels.create(theCnl.name, {
-                type: 'category'
-            });
-            const TxtChildren = await txtCnl.find({ parentID: theCnl._id });
-            const txts = TxtChildren.sort((a, b) => a.position - b.position);
-            txts.forEach(async (c) => {
-                await message.guild.channels.create(c.name, {
-                    type: 'text',
-                    parent: cat.id
+            const role = RoleData[x];
+            const newRole = message.guild.roles.cache.find(r => r.name === role.name);
+            if (!newRole) return;
+            const overwrits = await oData.find();
+            const roleFiltered = overwrits.filter(doc => doc.overwrites.some(o => o.id === role._id));
+            for (let index = 0; index < roleFiltered.length; index++) {
+                const document = roleFiltered[index];
+                let cOldData = await channelData.findOne({ _id: document._id });
+                if (!cOldData) cOldData = await txtCnl.findOne({ _id: document._id });
+                if (!cOldData) cOldData = await vcCnl.findOne({ _id: document._id });
+                if (!cOldData) return console.log('sex');
+                const channel = message.guild.channels.cache.find(c => c.name === cOldData.name);
+                const options = {};
+                let docover = document.overwrites.find(o => o.id === role._id);
+                new Discord.Permissions(docover.allow.bitfield).toArray().forEach(p => options[p] = true);
+                new Discord.Permissions(docover.deny.bitfield).toArray().forEach(p => options[p] = false);
+                await channel.updateOverwrite(newRole, options);
+                await overwrites.updateOne({ _id: document._id }, { $pull: { overwrites: docover } });
+                await overwrites.updateOne({ _id: document._id }, {
+                    $push: {
+                        overwrites: {
+                            id: newRole.id,
+                            type: 'role',
+                            allow: docover.allow,
+                            deny: docover.deny
+                        }
+                    }
                 });
-            });
-            const vcChildren = await vcCnl.find({ parentID: theCnl._id });
-            const vcs = vcChildren.sort((a, b) => a.position - b.position);
-            vcs.forEach(async (c) => {
-                await message.guild.channels.create(c.name, {
-                    type: 'voice',
-                    parent: cat.id
-                });
-            });
-            index = index + 1;
-        }, 5000);
+            }
+            x = x + 1;
+        }, 1000);
 
     }
 
