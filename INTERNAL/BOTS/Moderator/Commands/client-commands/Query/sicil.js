0ee7ee129fa92df41c851cdaff92db614572ecf5
Taurusian Tantoony <tantoony.id@gmail.com>
2021-09-22 00:19:37 +0300

    Update sicil.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Query/sicil.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Query/sicil.js
@@ -17,82 +17,84 @@ class Sicil extends Command {
             usage: "sicil @fero/ID ",
             examples: ["sicil @fero/ID"],
             category: "Sorgu",
-            aliases: ["sorgu","cezalar"],
+            aliases: ["sorgu", "cezalar"],
             accaptedPerms: ["root", "owner", "cmd-ceo", "cmd-double", "cmd-single"],
             cooldown: 60000,
         });
     }
 
     async run(client, message, args) {
-        
-    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-    if (!member) return message.react(client.emoji("error"));
-    let data = await sicil.findOne({ _id: member.user.id })
-    if (!data) return message.channel.send(`${member} kullanıcısının sicil verisi bulunamadı.`)
 
-    let config = {
-        border: {
-            bodyLeft: ``,
-            bodyRight: ``,
-            bodyJoin: `│`,
+        const emojis = await low(client.adapters('emojis'));
+        const channels = await low(client.adapters('channels'));
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+        if (!member) return message.react(client.emoji("error"));
+        let data = await sicil.findOne({ _id: member.user.id })
+        if (!data) return message.channel.send(`${member} kullanıcısının sicil verisi bulunamadı.`)
 
-            bottomJoin: '',
-            bottomLeft: '',
-            bottomRight: '',
-            bottomBody: '',
+        let config = {
+            border: {
+                bodyLeft: ``,
+                bodyRight: ``,
+                bodyJoin: `│`,
+
+                bottomJoin: '',
+                bottomLeft: '',
+                bottomRight: '',
+                bottomBody: '',
 
 
-            joinJoin: '',
-            joinLeft: '',
-            joinRight: '',
-            joinBody: '',
+                joinJoin: '',
+                joinLeft: '',
+                joinRight: '',
+                joinBody: '',
 
-            topBody: '',
-            topJoin: '',
-            topLeft: '',
-            topRight: '',
+                topBody: '',
+                topJoin: '',
+                topLeft: '',
+                topRight: '',
+            }
+        };
+
+        let karebar = [
+            ["ID", "Ceza", "Tarih", "Yetkili", "Sebep"]
+        ];
+
+        data.records.map(st => { karebar.push([st.id, `${st.type} ${st.punish}`, `${moment(st.created.getTime()).format("LLL")}`, client.users.cache.get(st.executor).tag, `${st.reason ? st.reason : "Belirtilmedi"}`]) })
+        let page = 1;
+        const question = await message.channel.send(` ${member} kullanıcısının sicil bilgileri aşağıda belirtilmiştir. Tekli cezaya bakmak için \`.cezasorgu ID\` yazınız. \`\`\`${table(karebar.slice(page == 1 ? 0 : page * 10 - 10, page * 10), config)}\`\`\``)
+
+
+        if (data && data.length > 10) {
+            question.react("◀");
+            question.react("❌");
+            question.react("▶");
+
+            const collector = question.createReactionCollector(
+                (react, user) => ["◀", "❌", "▶"].some((e) => e == react.emoji.name) && user.id == message.author.id,
+                { time: 120000 }
+            );
+
+            collector.on("collect", async (react) => {
+                await react.users.remove(message.author.id).catch(() => undefined);
+                if (react.emoji.name == "▶") {
+                    if (karebar.slice((page + 1) * 10 - 10, (page + 1) * 10).length <= 0) return;
+                    page += 1;
+                    let newList = table(karebar.slice(page == 1 ? 0 : page * 10 - 10, page * 10), config)
+                    question.edit(` ${member} kullanıcısının sicil bilgileri aşağıda belirtilmiştir. Tekli cezaya bakmak için \`.cezasorgu ID\` yazınız. \`\`\`${newList}\`\`\``);
+                }
+                if (react.emoji.name == "❌") {
+                    question.delete()
+                }
+                if (react.emoji.name == "◀") {
+                    if (karebar.slice((page - 1) * 10 - 10, (page - 1) * 10).length <= 0) return;
+                    page -= 1;
+                    let newList = table(karebar.slice(page == 1 ? 0 : page * 10 - 10, page * 10), config)
+                    question.edit(` ${member} kullanıcısının sicil bilgileri aşağıda belirtilmiştir. Tekli cezaya bakmak için \`.cezasorgu ID\` yazınız. \`\`\`${newList}\`\`\``);
+                }
+            });
         }
-    };
-
-    let karebar = [
-        ["ID", "Ceza", "Tarih", "Yetkili", "Sebep"]
-    ];
-
-    data.records.map(st => { karebar.push([st.id, `${st.type} ${st.punish}`, `${moment(st.created.getTime()).format("LLL")}`, client.users.cache.get(st.executor).tag, `${st.reason ? st.reason : "Belirtilmedi"}`]) })
-    let page = 1;
-    const question = await message.channel.send(` ${member} kullanıcısının sicil bilgileri aşağıda belirtilmiştir. Tekli cezaya bakmak için \`.cezasorgu ID\` yazınız. \`\`\`${table(karebar.slice(page == 1 ? 0 : page * 10 - 10, page * 10), config)}\`\`\``)
-
-
-    if (data && data.length > 10) {
-        question.react("◀");
-        question.react("❌");
-        question.react("▶");
-
-        const collector = question.createReactionCollector(
-            (react, user) => ["◀", "❌", "▶"].some((e) => e == react.emoji.name) && user.id == message.author.id,
-            { time: 120000 }
-        );
-
-        collector.on("collect", async (react) => {
-            await react.users.remove(message.author.id).catch(() => undefined);
-            if (react.emoji.name == "▶") {
-                if (karebar.slice((page + 1) * 10 - 10, (page + 1) * 10).length <= 0) return;
-                page += 1;
-                let newList = table(karebar.slice(page == 1 ? 0 : page * 10 - 10, page * 10), config)
-                question.edit(` ${member} kullanıcısının sicil bilgileri aşağıda belirtilmiştir. Tekli cezaya bakmak için \`.cezasorgu ID\` yazınız. \`\`\`${newList}\`\`\``);
-            }
-            if (react.emoji.name == "❌") {
-                question.delete()
-            }
-            if (react.emoji.name == "◀") {
-                if (karebar.slice((page - 1) * 10 - 10, (page - 1) * 10).length <= 0) return;
-                page -= 1;
-                let newList = table(karebar.slice(page == 1 ? 0 : page * 10 - 10, page * 10), config)
-                question.edit(` ${member} kullanıcısının sicil bilgileri aşağıda belirtilmiştir. Tekli cezaya bakmak için \`.cezasorgu ID\` yazınız. \`\`\`${newList}\`\`\``);
-            }
-        });
-    }
-    message.react(emojis.get("ok").value().split(':')[2].replace('>', ''));
+        message.react(emojis.get("ok").value().split(':')[2].replace('>', ''));
 
     }
 }
