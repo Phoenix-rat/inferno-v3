57e87f39ea42122eb4b57c535f31dfb83e936231
Taurusian Tantoony <tantoony.id@gmail.com>
2021-09-21 04:41:16 +0300

    Update supheac.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Registry/supheac.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Registry/supheac.js
@@ -28,18 +28,6 @@ class Supheac extends Command {
         if (!mentioned.roles.cache.has(roles.get("suspicious").value())) return await message.react(emojis.get("error").value().split(':')[2].replace('>', ''));
         await mentioned.roles.remove(roles.get("suspicious").value());
         await mentioned.roles.add(roles.get("welcome").value());
-        const embed = new Discord.MessageEmbed()
-            .setColor("#2f3136")
-            .setDescription(
-                stripIndents`
-            
-         ${emojis.get("pando1").value()} Aramıza hoş geldin **${mentioned.user.username}**
-    
-         ${emojis.get("pando2").value()} Seninle beraber **${rain(client, message.guild.memberCount)}** kişiyiz.
-    
-         ${emojis.get("pando3").value()} **Hesap:** ${rain(client, checkDays(mentioned.user.createdAt))} gün önce açılmıştır.
-        
-        `).setThumbnail(mentioned.user.displayAvatarURL());
         await message.guild.channels.cache.get(channels.get("welcome").value()).send(embed);
         await message.react(emojis.get("ok").value().split(':')[2].replace('>', ''));
         await message.guild.channels.cache.get(channels.get("mod-registry").value()).send(new Discord.MessageEmbed().setDescription(`${message.member} yetkilisi ${mentioned} kullanıcısının şüphesini kaldırdı.`));
