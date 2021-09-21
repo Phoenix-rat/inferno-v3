a42a1b2a502655fda1d6f8873bbfb1e84d8f2c44
Taurusian Tantoony <tantoony.id@gmail.com>
2021-09-21 03:44:45 +0300

    Update kayitsil.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Registry/kayitsil.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Registry/kayitsil.js
@@ -30,6 +30,29 @@ class KayitSil extends Command {
         await mentioned.roles.remove(mentioned.roles.cache.filter(r => r.editable).array());
         await mentioned.roles.add(roles.get('welcome').value());
         await message.react(emojis.get("ok").value().split(':')[2].replace('>', ''));
+        const aylar = [
+            "Ocak",
+            "Şubat",
+            "Mart",
+            "Nisan",
+            "Mayıs",
+            "Haziran",
+            "Temmuz",
+            "Ağustos",
+            "Eylül",
+            "Ekim",
+            "Kasım",
+            "Aralık"
+        ];
+        const tarih = new Date()
+        await message.guild.channels.cache.get(channels.get("kayıt_log").value()).send(new Discord.MessageEmbed().setDescription(stripIndents`
+        **Komutu kullanan:** ${message.member} (\`${message.member.user.id}\`)
+        **Kayıtsıza atılan::** ${mentioned} (\`${mentioned.user.id}\`)
+        **İsim/Yaş:** ${data.name} | ${data.age}
+        **Cinsiyet:** \`${data.sex === "Male" ? "Erkek" : "Kız"}\`
+        **Tag:** ${client.config.tag.some(t => mentioned.user.username.includes(t)) ? "\`Var\`" : "\`Yok\`"}
+        **Tarih:** \`${tarih.getDate()} ${aylar[tarih.getMonth()]} ${tarih.getFullYear()} ${tarih.getHours() + 3}:${tarih.getMinutes()}\`
+        `).setColor("#6be4a2").setAuthor(message.member.user.tag, message.member.user.displayAvatarURL({ dynamic: true })).setThumbnail(mentioned.user.displayAvatarURL({ dynamic: true })));
     }
 }
 module.exports = KayitSil;
