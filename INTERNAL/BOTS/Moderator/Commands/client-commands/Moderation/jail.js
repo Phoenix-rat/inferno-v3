d370a31541167a33f5f6bebe5d8513d2d491f762
Taurusian Tantoony <tantoony.id@gmail.com>
2021-09-22 03:04:20 +0300

    Update jail.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Moderation/jail.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Moderation/jail.js
@@ -20,11 +20,11 @@ class Jail extends Command {
         const emojis = await low(client.adapters('emojis'));
         const channels = await low(client.adapters('channels'));
         let mentioned = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-        if (!mentioned) return message.channel.send(new Discord.MessageEmbed().setDescription(`${emojis.get("kullaniciyok").value()} Kullanıcı bulunamadı!`).setColor('BLACK'));
+        if (!mentioned) return message.react(emojis.get("error").value().split(':')[2].replace('>', ''));
         let sebep = args.slice(1).join(" ");
-        if (!sebep) return message.channel.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`${emojis.get("soru").value()} Bir sebep girmelisin`));
-        if (message.member.roles.highest.rawPosition <= mentioned.roles.highest.rawPosition) return message.channel.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`${emojis.get("missingPerms").value()} Bunu yapmak için yeterli yetkiye sahip değilsin`));
-        if (!mentioned.bannable) return message.channel.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`${emojis.get("miisingBotPerms").value()} Bu kişiyi cezalandırmak için yeterli yetkiye sahip değilim`));
+        if (!sebep) return message.react(emojis.get("error").value().split(':')[2].replace('>', ''));
+        if (message.member.roles.highest.rawPosition <= mentioned.roles.highest.rawPosition) return message.react(emojis.get("error").value().split(':')[2].replace('>', ''));
+        if (!mentioned.bannable) return message.react(emojis.get("error").value().split(':')[2].replace('>', ''));
         client.extention.emit('Jail', mentioned, message.author.id, sebep, "perma", 0);
         if (mentioned.voice.channel) await mentioned.voice.kick();
         await message.react(emojis.get("ok").value().split(':')[2].replace('>', ''));
