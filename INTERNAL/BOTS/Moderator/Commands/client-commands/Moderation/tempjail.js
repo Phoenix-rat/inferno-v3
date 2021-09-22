82c982c8e44763815ddac762251006045c20112b
Taurusian Tantoony <tantoony.id@gmail.com>
2021-09-22 03:00:33 +0300

    jail

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Moderation/tempjail.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Moderation/tempjail.js
@@ -5,12 +5,12 @@ const { sayi } = require("../../../../../HELPERS/functions");
 class Jail extends Command {
     constructor(client) {
         super(client, {
-            name: "jail",
+            name: "tempjail",
             description: "Belirtilen kullanıcıyı hapise atar",
-            usage: "jail etiket/id sebep",
-            examples: ["jail 674565119161794560 10 gün botları kötü yapıyor"],
+            usage: "tempjail etiket/id sayı gün/saat sebep",
+            examples: ["tempjail 674565119161794560 10 gün botları kötü yapıyor"],
             category: "Moderasyon",
-            aliases: ["hapis", "zindan"],
+            aliases: ["tjail", "tj"],
             accaptedPerms: ["root", "owner", "cmd-ceo", "cmd-double", "cmd-single", "cmd-jail"],
             cooldown: 10000
         })
@@ -25,7 +25,10 @@ class Jail extends Command {
         if (!sebep) return message.channel.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`${emojis.get("soru").value()} Bir sebep girmelisin`));
         if (message.member.roles.highest.rawPosition <= mentioned.roles.highest.rawPosition) return message.channel.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`${emojis.get("missingPerms").value()} Bunu yapmak için yeterli yetkiye sahip değilsin`));
         if (!mentioned.bannable) return message.channel.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`${emojis.get("miisingBotPerms").value()} Bu kişiyi cezalandırmak için yeterli yetkiye sahip değilim`));
-        client.extention.emit('Jail', mentioned, message.author.id, sebep, "perma", 0);
+        if (!sayi(args[1])) return message.channel.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`${emojis.get("sayifalan").value()} Geçerli bir sayı girmelisin`));
+        if ((args[2] !== 'gün') && (args[2] !== 'saat')) return message.channel.send(new Discord.MessageEmbed().setDescription(`kullanım: ${this.info.usage}`));
+        if (args[2] === 'saat') args[1] = args[1] / 24;
+        client.extention.emit('Jail', mentioned, message.author.id, sebep, "temp", args[1]);
         if (mentioned.voice.channel) await mentioned.voice.kick();
         await message.react(emojis.get("ok").value().split(':')[2].replace('>', ''));
         //const logChannel = message.guild.channels.cache.get(channels.get("jaillog").value());
