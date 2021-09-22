25f169ea761ebd9dd43f9e003d70bde1d160250b
Taurusian Tantoony <tantoony.id@gmail.com>
2021-09-22 05:13:47 +0300

    a

--- INTERNAL/BOTS/Moderator/Commands/client-commands/General/cihaz.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/General/cihaz.js
@@ -21,16 +21,16 @@ class Booster extends Command {
         const roles = await low(client.adapters('roles'));
         const emojis = await low(client.adapters('emojis'));
         const mentioned = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-        if (!mentioned) return message.channel.send(new Discord.MessageEmbed().setDescription(`${emojis.get("kullaniciyok").value()} Kullanıcı bulunamadı!`).setColor('#2f3136'));
-        if (mentioned.user.id === message.member.user.id) return message.channel.send(new Discord.MessageEmbed().setDescription(`${emojis.get("pando1").value()} Kendi kendini etiketleme..`).setColor('#2f3136'));
-        if (member.user.presence.status == "offline") return message.inlineReply(`Belirlenen kullanıcı \`çevrimdışı\` olduğu için işlem iptal edildi.`)
+        if (!mentioned) return message.react(emojis.get("error").value().split(':')[2].replace('>', ''));
+        if (mentioned.user.id === message.member.user.id) return message.react(emojis.get("error").value().split(':')[2].replace('>', ''));
+        if (mentioned.user.presence.status == "offline") return message.react(emojis.get("error").value().split(':')[2].replace('>', ''));
 
         let adana = "Bilinmiyor";
         let ceyhan = Object.keys(mentioned.user.presence.clientStatus)
         if (ceyhan[0] == "desktop") adana = "Masaüstü Uygulama"
         if (ceyhan[0] == "web") adana = "İnternet Tarayıcısı"
         if (ceyhan[0] == "mobile") adana = "Mobil Telefon"
-        await message.channel.send(new Discord.MessageEmbed().setDescription(`${member} Kullanıcının şu anda kullandığı cihaz: \`${adana}\``));
+        await message.channel.send(new Discord.MessageEmbed().setDescription(`${mentioned} Kullanıcının şu anda kullandığı cihaz: \`${adana}\``));
 
     }
 }
