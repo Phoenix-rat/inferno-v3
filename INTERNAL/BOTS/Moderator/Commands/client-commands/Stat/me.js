0d8e719db1f5be69bc46ac7aca1a12dba0b2755e
Taurusian Tantoony <tantoony.id@gmail.com>
2021-09-26 14:32:50 +0300

    Update me.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/Stat/me.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/Stat/me.js
@@ -87,7 +87,7 @@ class Nerede extends Command {
         // ${emojis.get("statssh").value()} **Kayıt Odaları:** \`31 saat, 31 dakika\`
         // ${emojis.get("statssh").value()} **Private Odaları:** \`31 saat, 31 dakika\`
         // ${emojis.get("statssh").value()} **Eğlence Odaları:** \`31 saat, 31 dakika\``)
-            if(sesSira.length > 1) embed.addField(`Ses Kanalları`, stripIndents`${sesSira.map(sr => `<#${sr.id}> kanalında ${new Date(sr.duration).toISOString().substr(11, 8).toString().split(':').map((v, i) => v > 0 ? `${v} ${birim[i]}` : "").filter(str => str.length > 1).join(' ')}`).join('\n')}`)
+        //    if(sesSira.length > 1) embed.addField(`Ses Kanalları`, stripIndents`${sesSira.map(sr => `<#${sr.id}> kanalında ${new Date(sr.duration).toISOString().substr(11, 8).toString().split(':').map((v, i) => v > 0 ? `${v} ${birim[i]}` : "").filter(str => str.length > 1).join(' ')}`).join('\n')}`)
         // .addField(`Metin Kanalları`, `\`\`\`Burası Bakımda\`\`\``)
         // .addField(`Mesaj Kanalları`, `${emojis.get("statssh").value()} **Mesaj Kanalları:** \`${MesajVeri}\``).setTitle("Yetkili Stat Bilgi").setThumbnail(mentioned.user.displayAvatarURL({ dynamic: true }));
         
