41532ac00e9867865c4f1e27fb804f7f415e88e2
Taurusian Tantoony <tantoony.id@gmail.com>
2021-09-29 20:10:43 +0300

    Update nerede.js

--- INTERNAL/BOTS/Moderator/Commands/client-commands/General/nerede.js
+++ INTERNAL/BOTS/Moderator/Commands/client-commands/General/nerede.js
@@ -65,7 +65,7 @@ class Nerede extends Command {
         const durRecords = lastRecords.filter(r => r.channelID === mentioned.voice.channel.id);
         let duration = new Date().getTime() - entry.created.getTime();
         let p = 0;
-        if (entry.created.getTime() - 1000 < durRecords[0].exit.getTime()) durRecords.forEach((r, i) => {
+        if (durRecords[0] && (new Date().getTime() - 1000 < durRecords[0].exit.getTime() + duration)) durRecords.forEach((r, i) => {
             if (p < i) return;
             console.log(r);
             //320                           //375
